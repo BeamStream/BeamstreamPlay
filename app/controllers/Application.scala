67278b59971a8e5f701f68ec3e886aefcacc297a
Vikas Hazrati <vhazrati@inphina.com>
2012-01-30 14:43:44 +0530

    added a sample model and changed the view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.Quote
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your new application is ready.", new Quote("This test is specified by a honest person","vikas")))
   }
   
 }
