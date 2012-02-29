b125ebb0ac7f232057103d02e628b92d19fd2d87
Vikas Hazrati <vhazrati@inphina.com>
2012-02-29 11:28:53 +0530

    integrating twitter bootstrap with play 2.0

--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,20 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "BeamstreamPlay"
-    val appVersion      = "1.0"
+  val appName = "BeamstreamPlay"
+  val appVersion = "1.0"
 
-    val appDependencies = Seq(
-      "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
-      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
-      // Add your project dependencies here,
+  val appDependencies = Seq(
+    "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+    "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT" // Add your project dependencies here,
     )
+  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+  def customLessEntryPoints(base: File): PathFinder = (
+    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+    (base / "app" / "assets" / "stylesheets" * "*.less"))
 
-    val main = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, mainLang = SCALA).settings(
+   lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+ )
 
 }
