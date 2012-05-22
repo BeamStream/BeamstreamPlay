cfb687322237898c285a40287316c08b4a074d3f
AswathyRaju <awathy@toobler.com>
2012-05-22 15:15:24 +0530

    Login form

---
+++ public/client/js/models/loginmodel.js
@@ -0,0 +1,15 @@
+window.Login = Backbone.Model.extend({
+     
+        defaults: {
+        	
+            iam:null,
+            email:null,
+            pwdStatus:null,
+            password:null
+        },
+        
+        
+       
+});
+
+ 
