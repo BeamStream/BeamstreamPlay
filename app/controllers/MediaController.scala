0fd74c590c6386248e992cd43735e5b58846ede4
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-06-04 11:50:42 +0530

    Temp Commit

--- app/controllers/MediaController.scala
+++ app/controllers/MediaController.scala
@@ -1,15 +1,43 @@
 package controllers
 import play.api.mvc.Controller
 import play.api.mvc.Action
+import java.io.FileInputStream
+import java.io.File
 
 object MediaController extends Controller {
 
-  def getMedia = Action { implicit request =>
-    println("M here")
- val data=request.body.asMultipartFormData.get
-    println(data)
+  def getMedia = Action(parse.multipartFormData) { implicit request =>
 
-    Ok
+    println("I M here")
 
+    //     val body = request.body.asMultipartFormData
+    //      val picture = body.get.file("imageData")
+    //    
+    //      (picture.isEmpty) match {
+    //      case true => println("Kuch nhi h")
+    //      case _ => "Kuch h to sahi"
+    //    }
+
+    //     val imageData=request.body.file("imageData")
+    //   
+    //     println(imageData)
+
+    //      val fileObtained: File = imageData.asInstanceOf[File]
+    //     val inputStream = new FileInputStream(fileObtained)
+    //    println(inputStream)
+    
+    val aa = request.body.files
+    
+    println("Number of files sent: " + aa.size)
+    
+    request.body.file("imageData").map { picture =>
+
+      picture.ref.moveTo(new File("/home/neelkanth/Desktop"))
+      println("FileUploaded")
+      Ok
+    }.getOrElse {
+      println("No Result")
+      Ok
+    }
   }
 }
