d42f59980145cb896960d9e4c61bcce233dffb5a
AswathyRaju <awathy@toobler.com>
2012-06-04 15:34:43 +0530

    Mail Verification

---
+++ public/client/js/models/emailverificationmodel.js
@@ -0,0 +1,16 @@
+window.EmailVerification = Backbone.Model.extend({
+     
+        defaults: {
+        	
+            iam:null,
+            email:null,
+//            schoolName:null,
+//            answer:null,
+//            schoolRecord:null
+        },
+        
+        
+       
+});
+
+ 
