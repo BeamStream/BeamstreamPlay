05322d37a616e160b97d93a58fd230bb2143cb21
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-06-05 16:54:25 +0530

    Basic registration Entity modeled according to JSON

--- app/controllers/BasicRegistration.scala
+++ app/controllers/BasicRegistration.scala
@@ -14,18 +14,17 @@ import net.liftweb.json.{ parse, DefaultFormats }
 import net.liftweb.json.Serialization.{ read, write }
 import models.ResulttoSent
 import models.ResulttoSent
-
+import models.UserType
+import org.bson.types.ObjectId
 
 object BasicRegistration extends Controller {
 
   implicit val formats = DefaultFormats
 
- 
   /*
   * Basic Registration Permissions for a User  via Token authentication   
   */
 
- 
   def basicRegistration = Action { implicit request =>
 
     val tokenJSON = request.body.asFormUrlEncoded.get
@@ -44,30 +43,44 @@ object BasicRegistration extends Controller {
   }
 
   def basicRegistrationViaSocialSites(email: String, userName: String, firstName: String) = Action { implicit request =>
-   // Ok(views.html.basic_reg(basicRegForm, email, "1", userName, firstName))
+    // Ok(views.html.basic_reg(basicRegForm, email, "1", userName, firstName))
     Ok
   }
-  
-  
+
   /*
    * Registering a new User to Beamstream
    */
 
   def newUser = Action { implicit request =>
-    
-   val userJSON = request.body.asFormUrlEncoded.get
-   println(userJSON)
 
-//    basicRegForm.bindFromRequest.fold(
-//      errors => BadRequest(views.html.basic_reg(errors, "", "", "", "")),
-//      basicRegForm => {
-//
-//        val IdOfUserCreted = User.createNewUser(basicRegForm)
-//        val RegistrationSession = request.session + ("userId" -> IdOfUserCreted.toString)
-//        Redirect(routes.MessageController.messages).withSession(RegistrationSession)
-//      })
+    val userJSONMap = request.body.asFormUrlEncoded.get
+    val userJson = userJSONMap("data").toList(0)
+    val parsedUserJson=net.liftweb.json.parse(userJson)
     
-Ok
+    val iam = (parsedUserJson \ "iam").extract[String]
+    val emailId= (parsedUserJson \ "email").extract[String]
+    val schoolName=(parsedUserJson \ "schoolName").extract[String]
+    val userName=(parsedUserJson \ "userName").extract[String]
+    val password=(parsedUserJson \ "password").extract[String]
+    val firstName=(parsedUserJson \ "firstName").extract[String]
+    val lastName=(parsedUserJson \ "lastName").extract[String]
+    val location=(parsedUserJson \ "zipCode").extract[String]
+    val useCurrentLocation=(parsedUserJson \ "location").extract[Boolean]
+    
+    val user=new User(new ObjectId,UserType.apply(iam.toInt),emailId,firstName,lastName,userName,"",password,schoolName,location,List(),List(),List())
+    println(user)
+   // println(iam+""+emailId+""+schoolName+""+userName+""+password+""+firstName+""+lastName+""+location+""+useCurrentLocation)
+
+    //    basicRegForm.bindFromRequest.fold(
+    //      errors => BadRequest(views.html.basic_reg(errors, "", "", "", "")),
+    //      basicRegForm => {
+    //
+    //        val IdOfUserCreted = User.createNewUser(basicRegForm)
+    //        val RegistrationSession = request.session + ("userId" -> IdOfUserCreted.toString)
+    //        Redirect(routes.MessageController.messages).withSession(RegistrationSession)
+    //      })
+
+    Ok
   }
   /*
    * Send the verification mail to the User
@@ -81,7 +94,7 @@ Ok
 
     val iam = (userInformationJson \ "iam").extract[String]
     val emailId = (userInformationJson \ "email").extract[String]
-   
+
     SendEmail.sendEmail(emailId, iam)
 
     val jsonResponseToSent = new ResulttoSent("Success", "Email Sent Successfully")
