0955149a1ff62c18691371dcc253c32069b23b9e
AswathyRaju <awathy@toobler.com>
2012-06-06 18:31:46 +0530

    Stream Page with Userdata

---
+++ public/client/js/models/singleusermodel.js
@@ -0,0 +1,26 @@
+window.SingleUser = Backbone.Model.extend({
+     
+	 idAttribute: "_id",
+        defaults: {
+        	
+        	id:null,
+        	userType:null,
+        	email:null,
+        	firstName:null,
+        	lastName:null,
+        	userName:null,
+        	alias:null,
+        	password:null,
+        	orgName:null,
+        	location:null,
+        	streams:null,
+        	schoolId:null,
+        	classId:null
+        },
+//        url : "http://localhost/BeamstreamPlay/public/client/api.php",
+        url : "http://localhost:9000/loggedInUserJson",
+        
+       
+});
+ 
+ 
