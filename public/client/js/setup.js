fba60e555b34a8f4d3e2babf8b140721c68fbe08
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-06-22 11:28:26 +0530

    URL changes for cloud

--- public/client/js/setup.js
+++ public/client/js/setup.js
@@ -7,7 +7,7 @@
  /*
   * Config Variables for Url externalization
   */
- 
+ /*
 //For Login Page
 BS.login = "http://localhost:9000/users";
 
@@ -57,6 +57,62 @@ BS.newClass = "http://localhost:9000/newStream";
  BS.streamMessages = "http://localhost:9000/getAllMessagesForAStream";
  //expires the usersession
  BS.signOut ="http://localhost:9000/signOut";
+ 
+ */
+ 
+ /*
+  * For Cloud
+  */
+//For Login Page
+ BS.login = "http://beamstream-v3.herokuapp.com/users";
+
+ //For Verify Token
+ BS.verifyToken ="http://beamstream-v3.herokuapp.com/verifyToken";
+
+ //For Email Verification Page
+ BS.verifyEmail = "http://beamstream-v3.herokuapp.com/getEmailforNewUser";
+
+ //For Register New User Page
+ BS.registerNewUser = "http://beamstream-v3.herokuapp.com/registerNewUser";
+
+ //For Getting Logged in users basic details
+ BS.loggedInUserJson = "http://beamstream-v3.herokuapp.com/loggedInUserJson";
+
+ //For School registration Post Data
+ BS.saveSchool = "http://beamstream-v3.herokuapp.com/detailed_reg";
+
+ //GET  school details 
+ BS.schoolJson = "http://beamstream-v3.herokuapp.com/schoolJson";
+
+ //For Class registration Post Data
+ BS.saveClass = "http://beamstream-v3.herokuapp.com/class";
+
+ //For Profile Page Post Data
+ BS.saveProfile = "http://beamstream-v3.herokuapp.com/getMediafromPost";
+
+ //For populate list of class code
+ BS.autoPopulateClass  = "http://beamstream-v3.herokuapp.com/autoPopulateClasses";
+
+ //Get School name for a SchoolId
+ BS.schoolNamebyId = "http://beamstream-v3.herokuapp.com/getSchoolNamebyId";
+
+ //Get all schools under a user
+ BS.allSchoolForAUser = "http://beamstream-v3.herokuapp.com/getAllSchoolForAUser";
+
+ //For new class in class stream
+ BS.newClass = "http://beamstream-v3.herokuapp.com/newStream";
+
+ //Gel all streams of a user
+  BS.allStreamsForAUser ="http://beamstream-v3.herokuapp.com/allStreamsForAUser";
+
+ //Post message info
+  BS.postMessage ="http://beamstream-v3.herokuapp.com/newMessage";
+  
+ //Get all messages of a stream
+  BS.streamMessages = "http://beamstream-v3.herokuapp.com/getAllMessagesForAStream";
+  //expires the usersession
+  BS.signOut ="http://beamstream-v3.herokuapp.com/signOut";
+ 
 /* 
  * for testing in local
  */
