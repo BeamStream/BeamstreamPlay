e83dd0ab9e41b469612d8eee8a0b59a5a8bd1721
kishendas <kishen.das@gmail.com>
2012-06-24 20:55:52 -0700

    Visual File System

---
+++ app/models/Document.scala
@@ -0,0 +1,114 @@
+package models
+
+import com.novus.salat._
+import com.novus.salat.global._
+import com.novus.salat.annotations._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.MongoConnection
+import org.joda.time.DateTime
+import org.bson.types.ObjectId
+import utils.MongoHQConfig
+import net.liftweb.json.{ parse, DefaultFormats }
+import net.liftweb.json.Serialization.{ read, write }
+import java.util.Date
+import java.net.URL
+
+/**
+ * This class is used to store and retrieve all the information about documents.
+ * 
+ * @author Kishen 
+ */
+
+/*
+ * Enumeration for the document access 
+ * 
+ * Private - Only available for the owner
+ * Public - Available to all
+ * Restricted - Available to a restricted list of users
+ */
+object DocumentAccess extends Enumeration {
+  type DocumentAccess = Value
+  val Private = Value(0, "Private")
+  val Public = Value(1, "Public")
+  val Restricted = Value(1, "Restricted")
+  val Stream = Value(1,"Stream")
+
+}
+
+/*
+ * Enumeration for the document type
+ */
+object DocType extends Enumeration {
+  type DocType = Value
+  val GoogleDocs = Value(0, "GoogleDocs")
+  val YoutubeVideo = Value(1, "YoutubeVideo")
+  val Other = Value(3, "Other")
+}
+
+//Rockit
+//Document Type
+//Access 
+//Sorting 
+case class Document(@Key("_id") id: ObjectId, name: String, url: URL, docType: DocType.Value, userId: ObjectId, access: DocumentAccess.Value, streamId: ObjectId,
+  creationDate: Option[Date], lastUpdateDate: Option[Date], rocks: Int, rockers: List[ObjectId], comments : List[Message])
+
+case class DocumentForm(name: String)
+object Document {
+
+  implicit val formats = DefaultFormats
+
+  def allDocuments(): List[Document] = Nil
+
+/*
+ * Add a document
+ */
+  def addDocument(document : Document) {
+      DocumentDAO.insert(document)
+   }
+
+  /*
+   * Remove document
+   */
+  def removeDocument(document : Document) {
+    DocumentDAO.remove(document)
+
+  }
+
+  /*
+   * Find document by name
+   */
+
+  def findDocumentByName(name: String): List[Document] = {
+    val regexp = (""".*""" + name + """.*""").r
+    for (document <- DocumentDAO.find(MongoDBObject("name" -> regexp)).toList) yield document
+  }
+
+  /*
+   * Get all documents for a user
+   */
+  def getAllDocumentsforAUser(userId: ObjectId): List[Document] = {
+
+    val user = UserDAO.find(MongoDBObject("_id" -> userId)).toList(0)
+    //user.documents
+    null
+  }
+
+  /*
+   * Get all Documents List
+   */
+
+  def getAllDocuments(documentIdList: List[ObjectId]): List[Document] = {
+    var documentList: List[Document] = List()
+
+    for (documentId <- documentIdList) {
+      val doc = DocumentDAO.find(MongoDBObject("_id" -> documentId)).toList
+      documentList ++= doc
+    }
+
+    documentList
+  }
+
+}
+
+object DocumentDAO extends SalatDAO[Document, ObjectId](collection = MongoHQConfig.mongoDB("document"))
