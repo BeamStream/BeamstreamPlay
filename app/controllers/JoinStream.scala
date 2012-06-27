a7e89f9d5616b193f96454106a3192eed2d3d6e4
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-06-27 16:37:30 +0530

    Join stream functionality completed

--- app/controllers/JoinStream.scala
+++ app/controllers/JoinStream.scala
@@ -6,11 +6,24 @@ import models.Stream
 import play.api.data._
 import play.api.data.Forms._
 import org.bson.types.ObjectId
+import play.api.libs.json.DefaultFormat
+import net.liftweb.json.DefaultFormats
+import models.Class
 
 object JoinStream extends Controller {
 
+  implicit val formats=DefaultFormats
+  /*
+   * User Joins a Stream 
+   */
+  
   def joinStream = Action { implicit request =>
-    println(request.body)
+    val classListJsonMap = request.body.asFormUrlEncoded.get
+    val classJsonList = classListJsonMap("data").toList(0)
+    val classJson = net.liftweb.json.parse(classJsonList)
+    val classId = (classJson \ "id").extract[String]
+    val streamId=Class.findClasssById(new ObjectId(classId)).streams(0)
+    Stream.joinStream(streamId , new ObjectId(request.session.get("userId").get))
     Ok
   }
 
