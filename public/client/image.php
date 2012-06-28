3e306a1e823d27f09f03dd4d7bf406dce3d4613f
AswathyRaju <awathy@toobler.com>
2012-06-28 10:53:25 +0530

    Design Issues Fixes

---
+++ public/client/image.php
@@ -0,0 +1,14 @@
+<?php
+class Image
+{
+	function save()
+	{
+// 		print_R($_FILES);
+// 		print_R($_POST);
+		$arr = array('id' => 1, 'status' => 'success' , 'message'=>'Login suscces.');
+		echo json_encode($arr);
+	}
+}
+$Api = new Image();
+
+$Api->save();
