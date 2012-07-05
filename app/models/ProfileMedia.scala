7f77af47a9bc967476d66c0dfec5604e0f8bcb42
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-07-05 16:03:19 +0530

    Profile Photo and Video Json returned

--- app/models/ProfileMedia.scala
+++ app/models/ProfileMedia.scala
@@ -6,14 +6,22 @@ import com.novus.salat._
 import com.novus.salat.global._
 import com.novus.salat.annotations._
 import org.bson.types.ObjectId
+import com.mongodb.casbah.commons.MongoDBObject
 
-case class ProfileMedia(@Key("_id") id: ObjectId, userId: ObjectId, profileImageUrl:String ,profileVideoUrl:String , mobile: String, uploadType: String)
+case class ProfileMedia(@Key("_id") id: ObjectId, userId: ObjectId, profileImageUrl: String, profileVideoUrl: String, mobile: String, uploadType: String)
 object ProfileMedia {
-  
-  def saveMediaForUser(media:ProfileMedia){
-   val mediaId=ProfileMediaDAO.insert(media)
+
+  def saveMediaForUser(media: ProfileMedia) {
+    val mediaId = ProfileMediaDAO.insert(media)
+  }
+
+  /*
+ * Get Media for a user
+ */
+  def getMediaForAUser(userId: ObjectId): ProfileMedia = {
+  val mediaObtained=ProfileMediaDAO.find(MongoDBObject("userId" -> userId)).toList(0)
+  mediaObtained
   }
 
 }
-
 object ProfileMediaDAO extends SalatDAO[ProfileMedia, ObjectId](collection = MongoHQConfig.mongoDB("profileMedia"))
