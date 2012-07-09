c85e329fe20c9c0893a0237ebc34112648a4e3ed
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-07-09 16:38:39 +0530

    File Upload Functionality on Amazonintegrated

--- app/controllers/MediaController.scala
+++ app/controllers/MediaController.scala
@@ -43,27 +43,28 @@ object MediaController extends Controller {
     }.get
 
     // Fetch the video stream and details
-//    val videoComposite = request.body.file("videoData").map { videoData =>
-//      val videoAuthenticationToken = tokenEmail.securityToken
-//      val videoFilename = videoData.filename
-//      val contentType = videoData.contentType
-//      val videoFileObtained: File = videoData.ref.file.asInstanceOf[File]
-//      AmazonUpload.uploadFileToAmazon(videoFilename, videoFileObtained)
-//      videoURL="https://s3.amazonaws.com/Beamstream/"+videoFilename
-//      /*
-//      val profileVideo: File = videoData.ref.file.asInstanceOf[File]
-//      val profileVideoInputStream = new FileInputStream(profileVideo)
-//      new mediaComposite(videoFilename, contentType.get, profileVideoInputStream)
-//    */
-//    }.get
+    val videoComposite = request.body.file("videoData").map { videoData =>
+      val videoAuthenticationToken = tokenEmail.securityToken
+      val videoFilename = videoData.filename
+      val contentType = videoData.contentType
+      val videoFileObtained: File = videoData.ref.file.asInstanceOf[File]
+      AmazonUpload.uploadFileToAmazon(videoFilename, videoFileObtained)
+      videoURL="https://s3.amazonaws.com/Beamstream/"+videoFilename
+      /*
+      val profileVideo: File = videoData.ref.file.asInstanceOf[File]
+      val profileVideoInputStream = new FileInputStream(profileVideo)
+      new mediaComposite(videoFilename, contentType.get, profileVideoInputStream)
+    */
+    }.get
 
     val mediaJsonMap = request.body.asFormUrlEncoded.toList
     val uploadType = mediaJsonMap(0)._2.toList(0)
     val mobileNo = mediaJsonMap(1)._2.toList(0)
 
     // Save the profile picture for a user
-    val media = new ProfileMedia(new ObjectId, new ObjectId(request.session.get("userId").get), imageURL, videoURL, mobileNo, uploadType)
+    val media = new ProfileMedia(new ObjectId, new ObjectId(request.session.get("userId").get), imageURL, videoURL, mobileNo, uploadType,true)
     ProfileMedia.saveMediaForUser(media)
+    //ProfileMedia.isNotProfilePic( new ObjectId(request.session.get("userId").get))
     /*
     val mediaTransfrerObject = new MediaTransfer(new ObjectId(request.session.get("userId").get), MediaType.Image, true,
     imageComposite.inputStream, imageComposite.name, videoComposite.inputStream, videoComposite.name, mobileNo, uploadType)
