8993a4dd715fb325ff4142a6000e36a3a35a39b3
AswathyRaju <awathy@toobler.com>
2012-07-16 17:20:29 +0530

    cookies

--- public/client/js/router.js
+++ public/client/js/router.js
@@ -23,7 +23,7 @@ BS.AppRouter = Backbone.Router.extend({
     },
     initialize :function() {
     	
-    	// for Cookies 
+    	
     	if (typeof String.prototype.trimLeft !== "function") {
     	    String.prototype.trimLeft = function() {
     	        return this.replace(/^\s+/, "");
@@ -89,41 +89,9 @@ BS.AppRouter = Backbone.Router.extend({
   		
   		
     },
-    /*
-     * get cookies...
-     */
-    getCookies :function(){
-    	var c = document.cookie, v = 0, cookies = {};
-        if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
-            c = RegExp.$1;
-            v = 1;
-        }
-        if (v === 0) {
-            c.split(/[,;]/).map(function(cookie) {
-                var parts = cookie.split(/=/, 2),
-                    name = decodeURIComponent(parts[0].trimLeft()),
-                    value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
-                cookies[name] = value;
-            });
-        } else {
-            c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
-                var name = $0,
-                    value = $1.charAt(0) === '"'
-                              ? $1.substr(1, -1).replace(/\\(.)/g, "$1")
-                              : $1;
-                cookies[name] = value;
-            });
-        }
-        return cookies;
-
-    },
-    /*
-     * get a cookie.
-     */
     
-    getCookie : function(name) {
-        return this.getCookies()[name];
-    },
+    
+ 
 
     home: function() {
     	console.log('Here');
@@ -136,7 +104,7 @@ BS.AppRouter = Backbone.Router.extend({
      */
     
     login: function() {
-    	  $('#school-popup').children().detach(); 
+    	 $('#school-popup').children().detach(); 
     	 
          this.loginView = new BS.LoginView();
          this.loginView.render();
@@ -146,15 +114,13 @@ BS.AppRouter = Backbone.Router.extend({
          $(".checkbox").dgStyle();
          $(".signin_check").dgStyle();
          
-         
-         var username = this.getCookie("userName");
-         var username = this.getCookie("password");
- 
-         if(username != null && username != "" && password !=null && password != "") 
-         {
-        	 $('#email').val(username);
-        	 $('#password').val(password);
-         }
+//         var username=$.cookie("userName") ;
+//         var password = $.cookie("password") ;
+//         if(username != null && username != "" && password !=null && password != "") 
+//         {
+//        	 $('#email').val(username);
+//        	 $('#password').val(password);
+//         }
     
         
     },
