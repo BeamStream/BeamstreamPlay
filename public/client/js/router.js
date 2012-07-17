1bf8009dd74c35b90213f8f713165208cff662a6
AswathyRaju <awathy@toobler.com>
2012-07-17 09:57:39 +0530

    Related Cookies

--- public/client/js/router.js
+++ public/client/js/router.js
@@ -43,6 +43,7 @@ BS.AppRouter = Backbone.Router.extend({
     	    };
     	}
 
+
     	
     	var self = this;
         BS.user = new BS.SingleUser();
@@ -100,6 +101,38 @@ BS.AppRouter = Backbone.Router.extend({
     },
 
     /**
+     * get all cookies
+     */
+    getCookies :function () {
+        var c = document.cookie, v = 0, cookies = {};
+        if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
+            c = RegExp.$1;
+            v = 1;
+        }
+        if (v === 0) {
+            c.split(/[,;]/).map(function(cookie) {
+                var parts = cookie.split(/=/, 2),
+                    name = decodeURIComponent(parts[0].trimLeft()),
+                    value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
+                cookies[name] = value;
+            });
+        } else {
+            c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
+                var name = $0,
+                    value = $1.charAt(0) === '"'
+                              ? $1.substr(1, -1).replace(/\\(.)/g, "$1")
+                              : $1;
+                cookies[name] = value;
+            });
+        }
+        return cookies;
+    },
+
+    getCookie :function  (name) {
+        return this.getCookies()[name];
+    },
+    
+    /**
      * display login form
      */
     
@@ -114,13 +147,13 @@ BS.AppRouter = Backbone.Router.extend({
          $(".checkbox").dgStyle();
          $(".signin_check").dgStyle();
          
-//         var username=$.cookie("userName") ;
-//         var password = $.cookie("password") ;
-//         if(username != null && username != "" && password !=null && password != "") 
-//         {
-//        	 $('#email').val(username);
-//        	 $('#password').val(password);
-//         }
+         var username=this.getCookie("userName") ;
+         var password = this.getCookie("password") ;
+         if(username != null && username != "" && password !=null && password != "") 
+         {
+        	 $('#email').val(username);
+        	 $('#password').val(password);
+         }
     
         
     },
