2d266d8f729c3e0a02c5c3677fb4d1e6a6490fdf
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-08-17 10:05:57 +0530

    Comment Section Seperated

--- app/controllers/CommentController.scala
+++ app/controllers/CommentController.scala
@@ -24,12 +24,8 @@ object CommentController extends Controller {
     val commentPoster = User.getUserProfile(new ObjectId(request.session.get("userId").get))
     val comment = new Comment(new ObjectId, commentText, new Date, new ObjectId(request.session.get("userId").get),
       commentPoster.firstName, commentPoster.lastName, 0, List(), 0, List())
-
-    // Creating Comment & adding to message
-    // val commentId = Message.createMessage(comment)
-    // val commentObtained = Message.findMessageById(commentId)
     Message.addCommentToMessage(comment, new ObjectId(messageId))
-    Ok(write(List(comment)))
+    Ok(write(List(comment))).as("application/json")
 
   }
 
@@ -42,12 +38,6 @@ object CommentController extends Controller {
     val messageIdJSON = request.body.asFormUrlEncoded.get
     val messageId = messageIdJSON("messageId").toList(0)
     val message = Message.findMessageById(new ObjectId(messageId))
-    //    var commentsForAMessage: List[Message] = List()
-    //
-    //    for (commentsId <- message.comments) {
-    //      val comment = Message.findMessageById(commentsId)
-    //      commentsForAMessage ++= List(comment)
-    //    }
     val commentsForAMessage = message.comments
     println(write(commentsForAMessage))
     Ok(write(commentsForAMessage)).as("application/json")
