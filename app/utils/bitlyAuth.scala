d8891ad23da4eff2d3002067ce65867e483d833f
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-08-22 13:01:58 +0530

    Made the cloud setting dynamic and defined those in property file

--- app/utils/bitlyAuth.scala
+++ app/utils/bitlyAuth.scala
@@ -9,7 +9,7 @@ object bitlyAuth extends App {
    */
 
   def returnShortUrlViabitly(longUrl:String): String = {
-    val apiKey = Play.current.configuration.getString("bitly_apiKey").get
+    val apiKey = ConversionUtility.decodeMe(Play.current.configuration.getString("bitly_apiKey").get) // Encrypted Key
     val URL = "https://api-ssl.bitly.com/v3/shorten"
     val promise = WS.url(URL).setQueryParameter("apiKey", apiKey).setQueryParameter("login", "beamstream").setQueryParameter("longUrl", longUrl).get
     val shortUrlJson = promise.get.asJson.toString
