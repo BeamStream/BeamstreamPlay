aba130d05621c2def5cc580902671dcbb5168da4
AswathyRaju <awathy@toobler.com>
2012-08-22 15:11:47 +0530

    class stream-add new school

--- public/client/js/views/newschoolview.js
+++ public/client/js/views/newschoolview.js
@@ -3,7 +3,8 @@ BS.NewSchoolView = Backbone.View.extend({
 	events: {
 		"change select.graduated" : "showFields",
 		"change select.degreepgm" : "addOtherDegree",
-		"click #close-screen" : "closeScreen"
+		"click #close-screen" : "closeScreen",
+		"click #save": "saveNewSchool",
 	 },
 	
     initialize:function () {
@@ -76,6 +77,118 @@ BS.NewSchoolView = Backbone.View.extend({
      closeScreen :function(eventName){
     	 eventName.preventDefault(); 
     	 $('#new-school-view').children().detach(); 
-     }
+     },
+     /**
+      * save new school info
+      */
+     saveNewSchool :function(eventName){
+    	 eventName.preventDefault(); 
+    	 /* validation on other fields */
+     	var validate = jQuery('#new-school-form').validationEngine('validate');
+     	if(validate == true)
+     	{   
+     		/* put validation on "Graduated?" filed */
+         	var gStatus = true;
+         	$('select.graduated').each(function(index,item) {
+         		  
+         		 var Id='#'+item.id;
+         	     if($(Id).val() == "")
+         	     {
+         	       gStatus = false;
+         	        
+         	     }
+         	}); 
+         	if(gStatus == false)
+         	{
+         		$('#error').html("Please select your Graduation");
+         		
+         	}
+         	else
+         	{
+             	var  schoolDetails = this.getSchoolInfo();
+             	$.ajax({
+                     type: 'POST',
+                     url:BS.saveSchool,
+                     data:{data:schoolDetails},
+                     dataType:"json",
+                     success:function(data){
+                     	
+         				 // navigate to main stream page
+                    	 $('#new-school-view').children().detach();;
+//                    	 
+                    	_.each(data, function(info) {
+//                    	   $('#for-new-school').show();
+//                    	   $('#new-school').val(info.schoolName);
+//                      	   $('#new-school-id').val(info.assosiatedSchoolId.id);
+                      	   var classStream = new BS.ClassStreamView();
+                      	   classStream.getSchools();
+
+       		    	     });
+                         
+                     }
+                  });
+         	}
+     	}
+     	else
+     	{
+     		$('#error').html("Fields are not completely filled");
+     	}
+     	
+     },
+     /**
+      * get new school details from the form 
+      * @return School details as  JSON string
+      */
+     
+     getSchoolInfo:function (eventName) {
+   	    
+	    	  var schoolDetails = new Array();
+	    	  var i;
+	    	  var schoolId ='';
+	    	  var count = current;
+	    	  var schools = new BS.SchoolCollection();
+	    	
+	    	  
+	    	     var degreeexp,degdate,otherDegree;
+	
+		    	  /* display degree expected when selecting graduated as 'attending' or 'no' */
+		    	  if($('#graduated').val()== "attending" || $('#graduated').val()== "no")
+		    	  {
+			    	  degreeexp = $('#degree-expected').val();
+			    	  degdate = "";
+		    	  }
+		    	  /* display degree expected when selecting graduated as 'yes' */
+		    	  else if($('#graduated').val() == "yes")
+		    	  {
+			    	  degreeexp = "";
+			    	  degdate = $('#calendar').val();
+			    	  
+		    	  }
+		    	  else
+		    	  {
+			    	  graduated = "";
+			    	  degreeexp= "";
+		    	  }
+		    	  if($('#degreeprogram').val() == "Other")
+		    	  {
+		
+		    		  	otherDegree = $('#other-degree').val();
+		
+		    	  }
+		    	  else
+		    	  {
+		    		  otherDegree ="";
+		    	  }
+	
+	
+		    	  var school = new BS.School();
+		    	  // school.set({id:i,schoolName: $('#school-name-'+i).val(),assosiatedSchoolId:assosiatedSchoolId,year:{name: $('#year-'+i).val()}, degreeExpected:{name: degreeexp}, major: $('#major-'+i).val(), degree:{name: $('#degreeprogram-'+i).val() }, graduated: $('#graduated-'+i).val(), graduationDate: degdate ,otherDegree: otherDegree});
+		    	  school.set({id:1,schoolName: $('#school-name').val(),assosiatedSchoolId:1,year:{name: $('#year').val()}, degreeExpected:{name: degreeexp}, major: $('#major').val(), degree:{name: $('#degreeprogram').val() }, graduated: $('#graduated').val(), graduationDate: degdate ,otherDegree: otherDegree});
+		
+		    	  schools.add(school);
+	    	  
+	    	  var schoolinfo = JSON.stringify(schools);
+	    	  return schoolinfo;
+   	  },
 
 });
