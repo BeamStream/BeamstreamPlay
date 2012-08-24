54a2d2b8cd36eec2fb0a74917243d7a7209cf776
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-08-24 17:38:10 +0530

    Class Stream Page Finished with All functionality

--- app/utils/MongoHQConfig.scala
+++ app/utils/MongoHQConfig.scala
@@ -11,7 +11,7 @@ object MongoHQConfig {
   val mongoServer = Play.current.configuration.getString("mongoServer").get
   val mongoPort = Play.current.configuration.getString("mongoPort").get.toInt
   val databaseName = Play.current.configuration.getString("databaseName").get
-  val dbUserName = Play.current.configuration.getString("databaseName").get
+  val dbUserName = Play.current.configuration.getString("dbUserName").get
   val dbUserPassword = Play.current.configuration.getString("dbUserPassword").get
 
   //MongoDB   connection Setup either locally or Server
