ace6387b6816500f5941edbf4b05dedd2af9101d
AswathyRaju <awathy@toobler.com>
2012-09-04 09:52:00 +0530

    Changes

--- public/client/js/setup.js
+++ public/client/js/setup.js
@@ -11,7 +11,6 @@ Backbone.history.start();
 
 //BS.URLRequired ="http://localhost:9000";     // for cloud use BS.URLRequired ="http://beamstream-v3.herokuapp.com";
 BS.URLRequired ="http://alpha.beamstream.com";  
-//BS.URLRequired ="http://192.168.10.10:9000"; 
 
 // For Login Page
 BS.login = BS.URLRequired+"/users";
