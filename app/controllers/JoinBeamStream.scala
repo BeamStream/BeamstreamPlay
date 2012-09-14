4988a0311397c2dff1f2c9642caab4c6652763d4
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-09-14 14:27:57 +0530

    beta User regustration

--- app/controllers/JoinBeamStream.scala
+++ app/controllers/JoinBeamStream.scala
@@ -4,6 +4,9 @@ import play.api.mvc.Action
 import models.ResulttoSent
 import net.liftweb.json.{ parse, DefaultFormats }
 import net.liftweb.json.Serialization.{ read, write }
+import models.BetaUser
+import org.bson.types.ObjectId
+import models.UserType
 
 object JoinBeamStream extends Controller {
 
@@ -12,7 +15,12 @@ object JoinBeamStream extends Controller {
   def regsisterToBeamStreamBeta = Action { implicit request =>
     val userInfoJsonMap = request.body.asFormUrlEncoded.get
     val userInfoJson = userInfoJsonMap("data").toList(0)
-    Ok(write(new ResulttoSent("Success", "Beta User Regsitered")))
+    val parsedUserJson = net.liftweb.json.parse(userInfoJson)
+    val iam = (parsedUserJson \ "iam").extract[String]
+    val emailId = (parsedUserJson \ "email").extract[String]
+    val userToCreate = new BetaUser(new ObjectId, UserType.apply(iam.toInt), emailId)
+    val userId=BetaUser.addBetaUser(userToCreate)
+    Ok(write(new ResulttoSent("Success", "You are added to the Beta Users List")))
   }
 
 }
