c40f286879444ed6975ae6fc864c576d086f5440
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-10-04 13:18:02 +0530

    Extracting Frame From Video

--- app/utils/ExtractFrameFromVideo.scala
+++ app/utils/ExtractFrameFromVideo.scala
@@ -9,43 +9,47 @@ import com.xuggle.mediatool.MediaListenerAdapter
 import com.xuggle.mediatool.ToolFactory
 import com.xuggle.mediatool.event.IVideoPictureEvent
 import com.xuggle.xuggler.Global
+import java.io.ByteArrayOutputStream
+import java.awt.image.DataBufferByte
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
-object ExtractFrameFromVideo {
-  val SECONDS_BETWEEN_FRAMES = 1000
+object ExtractFrameFromVideo extends App {
+  def extractFrameFromVideo {
+    val inputFilename = "/home/neelkanth/Desktop/Tere-Naam-(2003)---DVD---Rip---ARAHAN-1.mp4"
+    val outputFilePrefix = "/home/neelkanth/Desktop/"
+    var count = 0
 
-  val inputFilename = "/home/neelkanth/Desktop/Tere-Naam-(2003)---DVD---Rip---ARAHAN-1.mp4"
-  val outputFilePrefix = "/home/neelkanth/Desktop/"
-  var count = 0
+    val mediaReader: IMediaReader = ToolFactory.makeReader(inputFilename)
 
-  val mediaReader: IMediaReader = ToolFactory.makeReader(inputFilename)
+    mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR)
 
-  mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR)
+    mediaReader.addListener(new ImageSnapListener)
 
-  mediaReader.addListener(new ImageSnapListener)
-
-  while (count < 1) {
-    mediaReader.readPacket()
-  }
-
-  class ImageSnapListener extends MediaListenerAdapter {
-
-    override def onVideoPicture(event: IVideoPictureEvent) = {
-      dumpImageToFile(event.getImage)
-      count = count + 1
+    while (count < 1) {
+      mediaReader.readPacket()
     }
 
-    def dumpImageToFile(image: BufferedImage): String = {
-      try {
-        val outputFilename = outputFilePrefix + System.currentTimeMillis + ".png"
-        ImageIO.write(image, "png", new File(outputFilename))
-        outputFilename
-      } catch {
-        case e =>
-          e.printStackTrace;
-          null
+    class ImageSnapListener extends MediaListenerAdapter {
+
+      override def onVideoPicture(event: IVideoPictureEvent) = {
+        val obtainedinputStream=dumpImageToFile(event.getImage)
+        count = count + 1
+      }
+
+      def dumpImageToFile(image: BufferedImage): InputStream= {
+        val baos: ByteArrayOutputStream = new ByteArrayOutputStream
+        println(baos.size+"Before")
+        ImageIO.write(image, "png", baos)
+        println(baos.size+"After")
+        val imageInByte = baos.toByteArray
+        val decodedInput : InputStream =new ByteArrayInputStream(imageInByte)
+        decodedInput
+        
       }
     }
 
   }
+  extractFrameFromVideo
 
 }
