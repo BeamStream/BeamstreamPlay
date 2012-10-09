8218197e24b5c9365b2527b100f4fd5e7b30108c
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-10-09 12:05:19 +0530

    Change Docment Title and description problem Resolved

--- app/models/Document.scala
+++ app/models/Document.scala
@@ -151,7 +151,6 @@ object Document {
    * Change the Title and description of a document
    */
   def updateTitleAndDescription(documentId: ObjectId, newName: String, newDescription: String) = {
-    println(newName,newDescription)
     val document = DocumentDAO.find(MongoDBObject("_id" -> documentId)).toList(0)
     DocumentDAO.update(MongoDBObject("_id" -> documentId), document.copy(documentDescription = newDescription,documentName = newName), false, false, new WriteConcern)
   }
