4d3cb00ef473c72d1abee301b177e9d8b8c4f09c
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-02 16:36:30 +0530

    CrocsDocs

---
+++ app/utils/CrocdocUpload.scala
@@ -0,0 +1,12 @@
+package utils
+import play.libs.WS
+import java.io.File
+
+
+object CrocdocUpload extends App {
+
+  
+  val file=new File("/home/neelkanth/Desktop/hello.docx")
+    //	val promise = WS.url("https://crocodoc.com/api/v2/document/upload").setQueryParameter("token", "xhE9cX02C5GYIuZeMJvHr1Pa").post()
+//        println(promise)
+}
