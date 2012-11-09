f90429ae214f079e5df99df4d1f116b5c2669781
AswathyRaju <awathy@toobler.com>
2012-11-09 10:46:17 +0530

    Changes

--- public/client/js/setup.js
+++ public/client/js/setup.js
@@ -9,13 +9,13 @@ Backbone.history.start();
 * Config Variables for Url externalization
 */
 
-//BS.URLRequired ="http://localhost:9000";    
+BS.URLRequired ="http://localhost:9000";    
  
  
 //BS.URLRequired ="http://alpha.beamstream.com";
  
 
-BS.URLRequired ="http://test.beamstream.com"
+//BS.URLRequired ="http://test.beamstream.com"
 
 	
 // For Login Page
