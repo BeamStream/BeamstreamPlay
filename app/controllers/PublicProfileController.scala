302c8800cbb90f9a7c8094c09b71fb32d823aa42
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-15 18:27:00 +0530

    Public profile Of A User

---
+++ app/controllers/PublicProfileController.scala
@@ -0,0 +1,43 @@
+package controllers
+import play.api.mvc.Controller
+import java.text.SimpleDateFormat
+import utils.ObjectIdSerializer
+import play.api.mvc.Action
+import models.Message
+import org.bson.types.ObjectId
+import net.liftweb.json.{ parse, DefaultFormats }
+import net.liftweb.json.Serialization.{ read, write }
+import models.User
+
+object PublicProfileController extends Controller {
+
+  implicit val formats = new net.liftweb.json.DefaultFormats {
+    override def dateFormatter = new SimpleDateFormat("dd/MM/yyyy")
+  } + new ObjectIdSerializer
+
+  /**
+   * All Public Messages For A User
+   */
+
+  def getAllPublicMessagesForAUser = Action { implicit request =>
+    val userIdJsonMap = request.body.asFormUrlEncoded.get
+    val userIdJson = userIdJsonMap("data").toList(0)
+    val parsedUserIdjson = net.liftweb.json.parse(userIdJson)
+    val userId = (parsedUserIdjson \ "userId").extract[String]
+    val messages = Message.getAllPublicMessagesForAUser(new ObjectId(userId))
+    Ok(write(messages)).as("application/json")
+  }
+
+  /**
+   * Get User Profile
+   */
+  def getPublicProfileOfAUser = Action { implicit request =>
+    val userIdJsonMap = request.body.asFormUrlEncoded.get
+    val userIdJson = userIdJsonMap("data").toList(0)
+    val parsedUserIdjson = net.liftweb.json.parse(userIdJson)
+    val userId = (parsedUserIdjson \ "userId").extract[String]
+    val user = User.getUserProfile(new ObjectId(userId))
+    Ok(write(User(user.id, user.userType, user.email, user.firstName, user.lastName.charAt(0).toString, user.userName, user.alias, "", user.orgName, user.location, user.socialProfile, List(), List(), List(), List(), List(), user.followers)))
+  }
+
+}
