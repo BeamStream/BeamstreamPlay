2c51a617b9ac9f4aa2d148babc38a7801be84c3c
AswathyRaju <awathy@toobler.com>
2012-11-19 14:43:34 +0530

    Valid Email Address

--- public/client/js/views/invitepeopleview.js
+++ public/client/js/views/invitepeopleview.js
@@ -32,8 +32,26 @@ BS.InvitePeopleView = Backbone.View.extend({
     	eventName.preventDefault(); 
     	var validate =  $("#invite-form").valid();
     	var mailIds = $('#friends_emailId').val();
-//    	if(validate == true)
-//    	{
+    	var mailArray = mailIds.split(',');
+         
+        var validEmailsStatus = true ;
+        var  validEmails = [];
+        var emailregex =/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
+
+        _.each(mailArray, function(mailId) {
+        	mailId = $.trim(mailId);
+        	 if(!mailId.match(emailregex))
+             {
+        		 validEmailsStatus = false ;
+        		 validEmails.push(mailId);
+             }
+        	  
+         });
+         console.log(validEmailsStatus);
+         console.log(validEmails);
+        
+    	if(validEmailsStatus == true)
+    	{
     		$.ajax({
 	   			type : 'POST',
 	   			url : BS.inviteUser,
@@ -45,12 +63,12 @@ BS.InvitePeopleView = Backbone.View.extend({
 	   				
 	   			}
    		    });
-//    	}
-//    	else
-//    	{
-//    		$('#display_message').fadeIn("medium").delay(2000).fadeOut('slow');
-//    		$('.error-msg').html("Please enter a vallid email address");
-//    	}
+    	}
+    	else
+    	{
+    		$('#display_message').fadeIn("medium").delay(2000).fadeOut('slow');
+    		$('.error-msg').html("Please enter a vallid email address");
+    	}
     },
     
     /**
