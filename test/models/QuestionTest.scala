e5fd0d341e7308f8e4c3e81cd8335caf8364f069
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-21 13:43:01 +0530

    Add Comment To Question test

--- test/models/QuestionTest.scala
+++ test/models/QuestionTest.scala
@@ -14,7 +14,7 @@ import java.util.Date
 class QuestionTest extends FunSuite with BeforeAndAfter {
 
   test("Create a Question & remove a Question") {
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
@@ -30,7 +30,7 @@ class QuestionTest extends FunSuite with BeforeAndAfter {
   }
 
   test("Get All Questions") {
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
@@ -40,9 +40,9 @@ class QuestionTest extends FunSuite with BeforeAndAfter {
     val anotherQuestionId = Question.addQuestion(anotherQuestion)
     assert(Question.getAllQuestions(List(anotherQuestionId, questionId)).size === 2)
   }
-  
+
   test("Find Question By Id") {
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
@@ -50,23 +50,23 @@ class QuestionTest extends FunSuite with BeforeAndAfter {
     val questionId = Question.addQuestion(question)
     val anotherQuestion = new Question(new ObjectId, "How Was the Day ?", userId, QuestionAccess.Public, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
     val anotherQuestionId = Question.addQuestion(anotherQuestion)
-    assert(Question.findQuestionById(anotherQuestionId).size===1)
+    assert(Question.findQuestionById(anotherQuestionId).size === 1)
   }
-  
+
   test("Rocking The Question And Getting The Rockers") {
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
     val question = new Question(new ObjectId, "How Was the Class ?", userId, QuestionAccess.Public, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
     val questionId = Question.addQuestion(question)
-    Question.rockTheQuestion(questionId,userId)
-    assert(Question.rockersNameOfAQuestion(questionId)===List("Neel"))
-    assert(Question.rockersNameOfAQuestion(questionId).size===1)
+    Question.rockTheQuestion(questionId, userId)
+    assert(Question.rockersNameOfAQuestion(questionId) === List("Neel"))
+    assert(Question.rockersNameOfAQuestion(questionId).size === 1)
   }
-  
-  test("Privacy Of The Question(For A Class , For A School)"){
-     val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+
+  test("Privacy Of The Question(For A Class , For A School)") {
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
@@ -76,12 +76,12 @@ class QuestionTest extends FunSuite with BeforeAndAfter {
     val anotherQuestionId = Question.addQuestion(anotherQuestion)
     val yetAnotherQuestion = new Question(new ObjectId, "How Was the Day ?", userId, QuestionAccess.PrivateToSchool, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
     val yetAnotherQuestionId = Question.addQuestion(yetAnotherQuestion)
-    assert(Question.getAllPrivateToAClassQuestionForAUser(userId).size==1)
-    assert(Question.getAllPrivateToASchoolQuestionForAUser(userId).size==2)
+    assert(Question.getAllPrivateToAClassQuestionForAUser(userId).size == 1)
+    assert(Question.getAllPrivateToASchoolQuestionForAUser(userId).size == 2)
   }
-  
-  test("Delete The Question"){
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+
+  test("Delete The Question") {
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
@@ -89,20 +89,34 @@ class QuestionTest extends FunSuite with BeforeAndAfter {
     val questionId = Question.addQuestion(question)
     val anotherQuestion = new Question(new ObjectId, "How Was the Day ?", userId, QuestionAccess.PrivateToSchool, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
     val anotherQuestionId = Question.addQuestion(anotherQuestion)
-    assert(Question.deleteQuestionPermanently(questionId,userId)===false)  //User Who Deletes The Question Is Not The Creator Of Question So Not Authorized To delete
-    assert(Question.deleteQuestionPermanently(anotherQuestionId,userId)===true)  //User Who Deletes The Question Is  The Creator Of Question So  Authorized To delete
+    assert(Question.deleteQuestionPermanently(questionId, userId) === false) //User Who Deletes The Question Is Not The Creator Of Question So Not Authorized To delete
+    assert(Question.deleteQuestionPermanently(anotherQuestionId, userId) === true) //User Who Deletes The Question Is  The Creator Of Question So  Authorized To delete
   }
 
-  test("Follow The Question"){
-    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(),List())
+  test("Follow The Question") {
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
     val userId = User.createUser(user)
     var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
     val streamId = Stream.createStream(stream)
     val question = new Question(new ObjectId, "How Was the Class ?", userId, QuestionAccess.PrivateToClass, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
     val questionId = Question.addQuestion(question)
-    assert(Question.findQuestionById(questionId).head.followers.size===0)
-    Question.followQuestion(userId,questionId)
-    assert(Question.findQuestionById(questionId).head.followers.size===1)
+    assert(Question.findQuestionById(questionId).head.followers.size === 0)
+    Question.followQuestion(userId, questionId)
+    assert(Question.findQuestionById(questionId).head.followers.size === 1)
+  }
+  
+  test("Add Comment To Question") {
+    val user = User(new ObjectId, UserType.Professional, "neel@knoldus.com", "Neel", "Sachdeva", "", "Neil", "Neel", "Knoldus", "", "", List(), List(), List(), List(), List(), List())
+    val userId = User.createUser(user)
+    var stream = Stream(new ObjectId, "al1pha", StreamType.Class, userId, List(userId), true, List("Tag1", "Tag2"))
+    val streamId = Stream.createStream(stream)
+    val question = new Question(new ObjectId, "How Was the Class ?", userId, QuestionAccess.PrivateToClass, streamId, "Neel", "Sachdeva", new Date, 0, List(), List(), List(), List())
+    val questionId = Question.addQuestion(question)
+    assert(Question.findQuestionById(questionId).head.comments.size === 0)
+    val comment = new Comment(new ObjectId, "Comment1", new Date, userId, user.firstName, user.lastName, 0, List(userId))
+    val commentId = Comment.createComment(comment)
+    Question.addCommentToQuestion(commentId, questionId)
+    assert(Question.findQuestionById(questionId).head.comments.size === 1)
   }
 
   after {
