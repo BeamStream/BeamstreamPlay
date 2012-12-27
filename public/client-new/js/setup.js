8dda768a240e7a833d1cbab33871e0de0603f2b4
AswathyRaju <awathy@toobler.com>
2012-12-27 16:09:02 +0530

    Poll options

--- public/client-new/js/setup.js
+++ public/client-new/js/setup.js
@@ -80,7 +80,7 @@ BS.bitly = BS.URLRequired+"/bitly";
 //POST comment
 BS.newComment = BS.URLRequired+"/newComment";
 //Get all comments of a message
-BS.allCommentsForAMessage = BS.URLRequired+"/allCommentsForAMessage";
+BS.allComments = BS.URLRequired+"/allComments";
 //for forgot password
 BS.forgotPassword = BS.URLRequired+"/forgotPassword";
 //sort messages
@@ -160,7 +160,7 @@ BS.newQuestion =BS.URLRequired+"/newQuestion";
 BS.rockQuestion =BS.URLRequired+"/rockQuestion";
 BS.followQuestion =BS.URLRequired+"/followQuestion";
 BS.giveMeRockersOfQuestion =BS.URLRequired+"/giveMeRockersOfQuestion";
-BS.getAllQuestionsOfAStream =BS.URLRequired+"/getAllQuestionsForAUser";
+BS.getAllQuestionsOfAStream =BS.URLRequired+"/getAllQuestionForAStream";
 
  
 
