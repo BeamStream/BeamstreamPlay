434fad078ca1cdec03e5926b030c01e0636cc628
Sanoop S <sanoop@toobler.com>
2013-01-22 17:53:30 +0530

    registration

--- public/client-new/js/models/usermodel.js
+++ public/client-new/js/models/usermodel.js
@@ -18,67 +18,45 @@
 
         BS.UserModel = Backbone.Model.extend({
     
-            defaults: {	        	
-//                iam:0
-//                mailid:null,
-//                userpassword:null,
-//                confirmpassword:null
-//                firstName:null,
-//                lastName:null,
-//                schoolName:null,
-//                major:null,
-//                aboutYourself:null,
-//                gradeLevel:null,
-//                degreeProgram:null,
-//                graduate:null,
-//                cellNumber:null,
-//                location:null
-	        },
-                
-                url:BS.verifyEmail,      
-                
-                    /**
-                    * model side validation
-                    */
+                defaults: {	        	
+                    iam:0
+    //                mailid:null,
+    //                userpassword:null,
+    //                confirmpassword:null
+    //                firstName:null,
+    //                lastName:null,
+    //                schoolName:null,
+    //                major:null,
+    //                aboutYourself:null,
+    //                gradeLevel:null,
+    //                degreeProgram:null,
+    //                graduate:null,
+    //                cellNumber:null,
+    //                location:null
+                    },
+
+                    url:BS.verifyEmail,      
+
+                        /**
+                        * model side validation
+                        */
                 validate: function(attrs, options) {
-
                     var email_filter    = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;                         
-//                                       var errors =[];
-                                       
-
-//                       
-//                                                         
-//                    var errors=new Array();
-
-
-                    if (!email_filter.test(attrs.mailid)) 
-                       {
-//                        errors.push({name: 'email', error: 'Please enter a valid email address'});
-                        return "No";
-
+                    var errors =[];
+                    if ((_.has(attrs,"mailid"))&&(!email_filter.test(attrs.mailid))) {
+                        errors.push({name: 'mailid', error: 'Please enter a valid email address'});
                         }
-//                        
-                        if (!attrs.userpassword){
-                        return "No";  
-                                }
-//                     errors.push({name: 'no inputs', error: 'Please enter correct inputs'});
-
-                        if(!attrs.confirmpassword) {
-                            return "No";
+    //                        
+                    if ( (_.has(attrs,"userpassword"))&&(attrs.userpassword==="") )  {  
+                        errors.push({name: 'userpassword', error: 'Please enter a valid email address'});
                         }
-                     
-                    
-//                    if (attrs.userpassword == '') {
-////                        errors.push({name: 'userpassword', error: 'Please enter a valid password'});
-//                        return "userpassword";             
-//                        }
-//                    if (attrs.confirmpassword == '') {
-////                        errors.push({name: 'confirmpassword', error: 'Please enter a valid confirmpassword'});
-//                        return "confirmpassword";        
-//                        }
-//                    return errors;
+                        
+                    if ( (_.has(attrs,"confirmpassword"))&&(attrs.confirmpassword==="") )  {  
+                        errors.push({name: 'confirmpassword', error: 'Please enter a valid email address'});
+                        }
 
-                    }
-                    
-    
+                    if ( errors.length ) return errors;
+
+                    }    
+
 });
