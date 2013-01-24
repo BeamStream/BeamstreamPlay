e149968821a7d667d2912cdf7dc99a06c85b701c
AswathyRaju <awathy@toobler.com>
2013-01-24 10:02:42 +0530

    New registrations

--- public/client-new/js/models/usermodel.js
+++ public/client-new/js/models/usermodel.js
@@ -20,51 +20,51 @@
     
         	defaults: {	        	
         		iam:0
-    //                mailid:null,
-    //                userpassword:null,
-    //                confirmpassword:null
-    //                firstName:null,
-    //                lastName:null,
-    //                schoolName:null,
-    //                major:null,
-    //                aboutYourself:null,
-    //                gradeLevel:null,
-    //                degreeProgram:null,
-    //                graduate:null,
-    //                cellNumber:null,
-    //                location:null
+//                mailid:null,
+//                userpassword:null,
+//                confirmpassword:null
+//                firstName:null,
+//                lastName:null,
+//                schoolName:null,
+//                major:null,
+//                aboutYourself:null,
+//                gradeLevel:null,
+//                degreeProgram:null,
+//                graduate:null,
+//                cellNumber:null,
+//                location:null
             },
 
             url:BS.verifyEmail,   
-//            urlRoot:'http://192.168.10.118:3000/test',
+//            urlRoot:'http://localhost/client/api.php',
                     
                     
             /* @ TODO .Will remove this after getting server methods */ 
-            sync: function(method, model, options){  
-                options.dataType = "jsonp";  
-                return Backbone.sync(method, model, options);  
-            },  
+//            sync: function(method, model, options){  
+//                options.dataType = "jsonp";  
+//                return Backbone.sync(method, model, options);  
+//            },  
 
             /**
             * model side validation
             */
             validate: function(attrs, options) {
+            	
                 var email_filter    = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;                         
                 var errors =[];
                 if ((_.has(attrs,"mailid"))&&(!email_filter.test(attrs.mailid))) {
                     errors.push({name: 'mailid', error: 'Please enter a valid email address'});
-                    }
-//                        
+                }
                 if ( (_.has(attrs,"userpassword"))&&(attrs.userpassword==="") )  {  
                     errors.push({name: 'userpassword', error: 'Please enter a valid email address'});
-                    }
+                }
                     
                 if ( (_.has(attrs,"confirmpassword"))&&(attrs.confirmpassword==="") )  {  
                     errors.push({name: 'confirmpassword', error: 'Please enter a valid email address'});
-                    }
+                }
 
                 if ( errors.length ) return errors;
 
-                }    
+            }    
 
-});
+        });
