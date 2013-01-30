cdbff395abb73b2180884dc60a400f46de0684eb
AswathyRaju <awathy@toobler.com>
2013-01-30 10:07:43 +0530

    Signup testing

--- public/client-new/js/model/user.js
+++ public/client-new/js/model/user.js
@@ -1,17 +1,39 @@
+/***
+* BeamStream
+*
+* Author                : Aswathy P.R (aswathy@toobler.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 29/January/2013
+* Description           : Backbone model for user details 
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
+
 define(['model/baseModel'], function(BaseModel) {
 	var User = BaseModel.extend({ 
 		objName: 'User',
-//		validation: {
-//			firstName: {
-//				required: true
-//			},
-//			lastName: {
-//				required: true
-//			},
-//			age: {
-//				required: true
-//			}
-//		}
+//		url: '/user', 
+		validation: {
+			mailId: {
+				required: true,
+				pattern: 'email'
+			} ,
+			password: {
+				required: true,
+				pattern: 'password'
+			},
+			confirmPassword: {
+				required: true,
+//				equalTo: 'password'
+			}
+		}
 
 	});
 	return User;
--- public/client-new/js/view/signupView.js
+++ public/client-new/js/view/signupView.js
@@ -1,16 +1,55 @@
-define(['view/baseView'], function(BaseView){
+/***
+* BeamStream
+*
+* Author                : Aswathy P.R (aswathy@toobler.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 29/January/2013
+* Description           : Backbone sign up page
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
+
+define(['view/formView' ,'model/user'], function(FormView , UserModel){
 	var signupView;
-	signupView = BaseView.extend({
+	signupView = FormView.extend({
 		objName: 'signupView',
-//		events:{
-//			'click .headline': 'clickRow'
-//		},
-//		clickRow: function(e){
-//			$(e.target).css({'color':'red'});
-//		}
-
-
 		
+		events:{
+			'click #registeration': 'registration'
+		},
+				
+		onAfterInit: function(){
+			this.data.reset();
+		},
+
+		/**
+		 * @TODO  user registration 
+		 */
+		registration:function(e){
+			e.preventDefault();
+			var user = new UserModel();
+			
+			user.set({
+				mailId :  $('#mailid').val(),
+				password :  $('#password').val(),
+				confirmPassword : $('#confirmPassword').val(),
+				 
+			});
+			
+			var userDetails = JSON.stringify(user);
+			console.log(userDetails);
+			user.save();
+			
+		},
+		
+ 
 	})
 	return signupView;
 });
