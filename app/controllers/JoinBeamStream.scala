b5f095f58977a495e7963e697b2ab2634e9658f6
Neelkanth Sachdeva <neelkanth@knoldus.com>
2013-01-31 15:05:38 +0530

    Beta user registration via Play Templating

--- app/controllers/JoinBeamStream.scala
+++ app/controllers/JoinBeamStream.scala
@@ -1,6 +1,5 @@
 package controllers
-import play.api.mvc.Controller
-import play.api.mvc.Action
+import play.api.mvc._
 import models.ResulttoSent
 import net.liftweb.json.{ parse, DefaultFormats }
 import net.liftweb.json.Serialization.{ read, write }
@@ -8,15 +7,48 @@ import models.BetaUser
 import org.bson.types.ObjectId
 import models.UserType
 import actors.UtilityActor
+import play.api.data._
+import play.api.data.Forms._
 
 object JoinBeamStream extends Controller {
 
   implicit val formats = DefaultFormats
 
   /**
+   * Beta User Registration Form
+   */
+  val betaUserRegsitrationForm = Form("email" -> email)
+
+  /**
+   *  Beta User Page Rendering
+   */
+
+  def betaUserRegistration = Action {
+    Ok(views.html.betaUser(betaUserRegsitrationForm, ""))
+  }
+
+  /**
+   *  Beta Users Registration
+   */
+  def regsisterToBeamStreamBetaviaTeplate = Action { implicit request =>
+
+    betaUserRegsitrationForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.betaUser(errors, "")),
+      betaUserRegsitrationForm => {
+        println(request.body.asFormUrlEncoded.get("email").toList(0))
+        UtilityActor.sendMailWhenBetaUserRegisters(request.body.asFormUrlEncoded.get("email").toList(0))
+        Ok(views.html.betaUser(JoinBeamStream.betaUserRegsitrationForm,  "Congratulations! You've been added to the Beamstream's beta users list"))
+      })
+
+  }
+
+  
+  
+  /**
    *  Beta Users Registration
    */
   def regsisterToBeamStreamBeta = Action { implicit request =>
+
     val userInfoJsonMap = request.body.asFormUrlEncoded.get
     val emailId = userInfoJsonMap("email").toList(0)
     val userToCreate = new BetaUser(new ObjectId, emailId)
@@ -29,7 +61,5 @@ object JoinBeamStream extends Controller {
         UtilityActor.sendMailWhenBetaUserRegisters(userToCreate.emailId)
         Ok(write(new ResulttoSent("Success", "Congratulations! You've been added to the Beamstream's beta users list")))
     }
-
   }
-
 }
