798eb661762b6e40e0d238614d9d294aabb9580c
Neelkanth Sachdeva <neelkanth@knoldus.com>
2013-02-18 13:40:04 +0530

    Checking Ok response

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -11,7 +11,7 @@ object Registration extends Controller {
   def registration(token: String, userId: String) = Action {
 
     (Token.findToken(token).isEmpty) match {
-      case false => Ok(views.html.registration(userId))
+      case false => Ok(views.html.registration())
       case true => Ok("Token has been expired")
     }
 
