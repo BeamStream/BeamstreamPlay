d70668abc71e5f01e4e634e4ef420aa9664ff821
Neelkanth Sachdeva <neelkanth@knoldus.com>
2013-02-22 16:21:45 +0530

    Registration Page Integration

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -28,29 +28,29 @@ object Registration extends Controller {
    * User Registration In Detail (RA)
    */
   def registerUser = Action { implicit request =>
-    //    try {
-    val jsonReceived = request.body.asJson.get
-    val userId = (jsonReceived \ "userId").as[String]
-    val firstName = (jsonReceived \ "firstName").as[String]
-    val lastName = (jsonReceived \ "lastName").as[String]
-    val schoolName = (jsonReceived \ "schoolName").as[String]
-    val major = (jsonReceived \ "major").as[String]
-    val gradeLevel = (jsonReceived \ "gradeLevel").as[String]
-    val degreeProgram = (jsonReceived \ "degreeProgram").as[String]
-    val graduate = (jsonReceived \ "graduate").as[String]
-    val location = (jsonReceived \ "location").as[String]
-    val about = (jsonReceived \ "aboutYourself").as[String]
-    val cellNumber = (jsonReceived \ "cellNumber").as[String]
-    User.updateUser(new ObjectId(userId), firstName, lastName, location, about, cellNumber)
+    try {
+      val jsonReceived = request.body.asJson.get
+      val userId = (jsonReceived \ "userId").as[String]
+      val firstName = (jsonReceived \ "firstName").as[String]
+      val lastName = (jsonReceived \ "lastName").as[String]
+      val schoolName = (jsonReceived \ "schoolName").as[String]
+      val major = (jsonReceived \ "major").as[String]
+      val gradeLevel = (jsonReceived \ "gradeLevel").as[String]
+      val degreeProgram = (jsonReceived \ "degreeProgram").as[String]
+      val graduate = (jsonReceived \ "graduate").as[String]
+      val location = (jsonReceived \ "location").as[String]
+      val about = (jsonReceived \ "aboutYourself").as[String]
+      val cellNumber = (jsonReceived \ "cellNumber").as[String]
+      User.updateUser(new ObjectId(userId), firstName, lastName, location, about, cellNumber)
 
-    val schoolId = School.addNewSchool(new School(new ObjectId, schoolName, ""))
-    val userSchool = new UserSchool(new ObjectId, schoolId, Year.withName(gradeLevel), Degree.withName(degreeProgram), major, Graduated.withName(graduate),
-      None, None, "", List())
-    UserSchool.createSchool(userSchool)
-    User.addInfo(List(userSchool), new ObjectId(userId))
-    Ok(write("Registration Successful")).as("application/json")
-    //    } catch {
-    //      case ex => Ok("Oops some error occurred")
-    //    }
+      val schoolId = School.addNewSchool(new School(new ObjectId, schoolName, ""))
+      val userSchool = new UserSchool(new ObjectId, schoolId, Year.withName(gradeLevel), Degree.withName(degreeProgram), major, Graduated.withName(graduate),
+        None, None, "", List())
+      UserSchool.createSchool(userSchool)
+      User.addInfo(List(userSchool), new ObjectId(userId))
+      Ok(write("Registration Successful")).as("application/json")
+    } catch {
+      case ex => Ok(write("Oops there were errors during registration")).as("application/json")
+    }
   }
 }
