fa3d725e5e5f4b3d5bc5542718a37335b19ba952
AswathyRaju <aswathy@toobler.com>
2013-02-23 18:11:41 +0530

    Registration -School Autopopulate

---
+++ public/client-new/js/view/addNewSchoolView.js
@@ -0,0 +1,41 @@
+/***
+* BeamStream
+*
+* Author                : Aswathy P.R (aswathy@toobler.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 23/February/2013
+* Description           : View for adding a new school to beamstream
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
+
+define(['view/formView'], function(FormView){
+	var newSchoolView;
+	newSchoolView = FormView.extend({
+		objName: 'newSchoolView',
+		
+		events:{
+	         'click #addSchool' : 'addNewSchool'
+		},
+
+		onAfterInit: function(){	
+			this.data.reset();
+	        
+        },
+        
+        addNewSchool: function(e){
+        	e.preventDefault();
+        	this.saveForm();
+        }
+        
+ 
+	})
+	return newSchoolView;
+});
