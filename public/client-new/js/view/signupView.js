0f5ffe4b19651144cae852337f3e38418efec44e
AswathyRaju <aswathy@toobler.com>
2013-02-26 18:26:38 +0530

    signup

--- public/client-new/js/view/signupView.js
+++ public/client-new/js/view/signupView.js
@@ -22,85 +22,83 @@ define(['view/formView'], function(FormView){
 		objName: 'signupView',
 		
 		events:{
-                    'click .menu-pic':'getUserTypeValue',
-                    'click #registeration': 'registration',
-                    'click .lastblock a' : 'socialMediaSignup',
-                    'keypress #password' : 'clearConfirmPasswordField'
+            'click .menu-pic':'getUserTypeValue',
+            'click #registeration': 'registration',
+            'click .lastblock a' : 'socialMediaSignup',
+            'keypress #password' : 'clearConfirmPasswordField'
 		},
 
 		onAfterInit: function(){	
-                    this.data.reset();
-                    $('.sign-tick').hide(); 
-                    $('.sign-close').hide(); 	        
-                },
+            this.data.reset();
+            $('.sign-tick').hide(); 
+            $('.sign-close').hide(); 	        
+        },
         
-                onBeforeRender:function(){
-                    console.log("testing in signup");
-                },
+         
                             
-                    /**
-                    * @TODO  user registration 
-                    */
-                registration:function(e){	
-                    e.preventDefault();
-            
-                    // @TODO validation - save only when user enter mailid ,password, confirmPassword
-                    if($('#mailid').val()&&$('#password').val()&&$('#confirmPassword').val()){  
-                        this.data.models[0].set('iam',$("#usertype").val());
-                        this.saveForm( );
-                    }  
+        /**
+        * @TODO  user registration 
+        */
+        registration:function(e){	
+            e.preventDefault();
+    
+            // @TODO validation - save only when user enter mailid ,password, confirmPassword
+            if($('#mailid').val()&&$('#password').val()&&$('#confirmPassword').val()){  
+                this.data.models[0].set('iam',$("#usertype").val());
+                this.saveForm( );
+            }  
 
-                    $('#mailid').val('');   
-                    $('#password').val('');
-                    $('#confirmPassword').val('');        
-                },
+            $('#mailid').val('');   
+            $('#password').val('');
+            $('#confirmPassword').val('');        
+        },
 
           
-                    /**
-                    * on form save success
-                    */
+        /**
+        * on form save success
+        */
 		success: function(model, data){
     	   
-                    var self = this;
-                    $('#mailId').val('');
-	
-                    if(data.status == 'Success')
-                    {
-                            alert('Signup successfull \n\nPlease check your mail.');
-                    }
-                    else
-                    {
-                            alert(data.message);
-                    }		
+            var self = this;
+            $('#mailId').val('');
+
+            if(data.status == 'Success')
+            {
+                alert('Signup successfull \n\nPlease check your mail.');
+            }
+            else
+            {
+                alert(data.message);
+            }		
 
 		},
                 
-                    /**
-                    * Method to set the value of "iam"
-                    */
-                getUserTypeValue:function(eventName){
-                    eventName.preventDefault();
+        /**
+        * Method to set the value of "iam"
+        */
+        getUserTypeValue:function(eventName){
+            eventName.preventDefault();
 
-                    $('.menu-pic div.active').removeClass('active');
-                    $(eventName.currentTarget).find('div').addClass('active');
+            $('.menu-pic div.active').removeClass('active');
+            $(eventName.currentTarget).find('div').addClass('active');
 
-
-                    $("#usertype").val(eventName.currentTarget.id);	
+            $("#usertype").val(eventName.currentTarget.id);	
 		},
 		
-                    /**
-                     *  sign up via social media 
-                     */
+        /**
+         *  sign up via social media 
+         */
 		socialMediaSignup: function(e){
-                    e.preventDefault();
-                    console.log($(e.target).parents('a').attr('id'));
+            e.preventDefault();
+            console.log($(e.target).parents('a').attr('id'));
 		},
 		
-                    /**
-                    * clear confirm password field when we change the password
-                    */
+		
+        /**
+        * clear confirm password field when we change the password
+        */
 		clearConfirmPasswordField: function(){
-                    $('#confirmPassword').val('');
+            $('#confirmPassword').val('');
 		}
  
 	})
