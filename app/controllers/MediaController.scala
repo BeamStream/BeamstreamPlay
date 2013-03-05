6987d758c5648f4ddbb19174727af07ccaaeaf24
Neelkanth Sachdeva <neelkanth@knoldus.com>
2013-03-05 16:53:07 +0530

    Photo Upload

--- app/controllers/MediaController.scala
+++ app/controllers/MediaController.scala
@@ -139,7 +139,7 @@ object MediaController extends Controller {
       val imageURL = "https://s3.amazonaws.com/BeamStream/" + imageNameOnAmazon
       val media = new UserMedia(new ObjectId, imageFilename, "", new ObjectId(request.session.get("userId").get), new Date, imageURL, UserMediaType.Image, DocumentAccess.Public, true, "", 0, List(), List(), 0)
       UserMedia.saveMediaForUser(media)
-      Ok(views.html.classpage())
+      Ok(write(new ResulttoSent("Success", "Profile Photo Uploaded Successfully"))).as("application/json")
     } catch {
       case exception => InternalServerError("Oops , Photo Upload Unsuccessful")
     }
