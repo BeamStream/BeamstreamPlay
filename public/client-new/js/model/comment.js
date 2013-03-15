37f0abf93dfeb3509331eb582af6928d5ac20a11
AswathyRaju <aswathy@toobler.com>
2013-03-15 18:27:07 +0530

    Comment

---
+++ public/client-new/js/model/comment.js
@@ -0,0 +1,27 @@
+/***
+* BeamStream
+*
+* Author                : Aswathy P.R (aswathy@toobler.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 15/March/2013
+* Description           : Backbone model for message comments
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
+
+define(['model/baseModel'], function(BaseModel) {
+	var Comment = BaseModel.extend({ 
+		objName: 'Comment',
+		url:'/newComment',
+
+	});
+        
+	return Comment;
+});
