1597d3be630610c64d65565ad2e1f8784cf25f58
AswathyRaju <aswathy@toobler.com>
2013-04-01 12:08:32 +0530

    Recover Password

---
+++ public/client-new/js/model/recoverPassword.js
@@ -0,0 +1,35 @@
+/***
+* BeamStream
+*
+* Author                : Aswathy P.R (aswathy@toobler.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 27/March/2013
+* Description           : to recover password
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
+define(['model/user'], function(User) {
+	var RecoverPassword = User.extend({
+		
+		defaults: {
+			mailId : ''
+        },
+		validation: {
+
+			mailId: {
+				required: true,
+				pattern: 'email'
+			} 
+		}
+		
+	});
+		        
+	return RecoverPassword;
+});
