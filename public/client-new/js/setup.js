35a978ddfce95cbfc1e2b2583486cb5aa50b02b1
AswathyRaju <aswathy@toobler.com>
2013-04-10 09:16:10 +0530

    Deploy to alpha

--- public/client-new/js/setup.js
+++ public/client-new/js/setup.js
@@ -9,12 +9,12 @@ Backbone.history.start();
 * Config Variables for Url externalization
 */
 
-BS.URLRequired ="http://localhost:9000";   
+//BS.URLRequired ="http://localhost:9000";   
 
 //BS.URLRequired ="http://192.168.2.31:9000";   
 
 
-//BS.URLRequired ="http://www.beamstream.com";
+BS.URLRequired ="http://www.beamstream.com";
 
 
 //BS.URLRequired ="http://test.beamstream.com"
