bbfc3ee2f6bb16c425f708cd288392b501d865d3
neelsachdeva <neelkanthsachdeva@gmail.com>
2013-05-09 12:42:50 +0530

    #955 Fixed

--- app/controllers/PublicProfileController.scala
+++ app/controllers/PublicProfileController.scala
@@ -20,7 +20,7 @@ object PublicProfileController extends Controller {
    * All Public Messages For A User
    */
 
-  def getAllPublicMessagesForAUser(userId:String) = Action { implicit request =>
+  def getAllPublicMessagesForAUser(userId: String) = Action { implicit request =>
     val messages = Message.getAllPublicMessagesForAUser(new ObjectId(userId))
     Ok(write(messages)).as("application/json")
   }
@@ -28,21 +28,28 @@ object PublicProfileController extends Controller {
   /**
    * Get User Profile
    */
-//  def getPublicProfileOfAUser = Action { implicit request =>
-//    val userIdJsonMap = request.body.asFormUrlEncoded.get
-//    val userIdJson = userIdJsonMap("data").toList(0)
-//    val parsedUserIdjson = net.liftweb.json.parse(userIdJson)
-//    val userId = (parsedUserIdjson \ "userId").extract[String]
-//    val user = User.getUserProfile(new ObjectId(userId))
-//    Ok(write(User(user.id, user.userType, user.email, user.firstName, user.lastName.charAt(0).toString, user.userName, user.alias, "", user.orgName, user.location, user.socialProfile, List(), List(), List(), List(), List(), user.followers)))
-//  }
+  //  def getPublicProfileOfAUser = Action { implicit request =>
+  //    val userIdJsonMap = request.body.asFormUrlEncoded.get
+  //    val userIdJson = userIdJsonMap("data").toList(0)
+  //    val parsedUserIdjson = net.liftweb.json.parse(userIdJson)
+  //    val userId = (parsedUserIdjson \ "userId").extract[String]
+  //    val user = User.getUserProfile(new ObjectId(userId))
+  //    Ok(write(User(user.id, user.userType, user.email, user.firstName, user.lastName.charAt(0).toString, user.userName, user.alias, "", user.orgName, user.location, user.socialProfile, List(), List(), List(), List(), List(), user.followers)))
+  //  }
 
   /**
    * Get All Public Documents For A User
    */
-  def getAllPublicDocumentForAUser(userId:String) = Action { implicit request =>
+  def getAllPublicDocumentForAUser(userId: String) = Action { implicit request =>
     val publicDocsForAUser = Document.getAllPublicDocumentForAUser(new ObjectId(userId))
     Ok(write(publicDocsForAUser)).as("application/json")
   }
 
+  /**
+   * ******************************************Rearchitecture*************************************************
+   */
+
+  def renderProfilePage = Action { implicit request =>
+    Ok(views.html.profile())
+  }
 }
