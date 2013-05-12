e0dd9d11bcc1ad84fda5805071ce078d7112da90
Daniel Hew <danielhew@hotmail.com>
2013-05-11 21:53:48 -0700

    undoing bad checkin

--- app/utils/MongoHQConfig.scala
+++ app/utils/MongoHQConfig.scala
@@ -7,20 +7,14 @@ object MongoHQConfig {
   /**
    * This all will come from "application.conf" file
    */
-//  val mongoServer = Play.current.configuration.getString("mongoServer").get
-//  val mongoPort = Play.current.configuration.getString("mongoPort").get.toInt
-//  val databaseName = Play.current.configuration.getString("databaseName").get
-//  val dbUserName = Play.current.configuration.getString("dbUserName").get
-//  val dbUserPassword = Play.current.configuration.getString("dbUserPassword").get
-//
-//  val mongoDB = MongoConnection(mongoServer, mongoPort)(databaseName)
-//  mongoDB.authenticate(dbUserName, dbUserPassword)
+  val mongoServer = Play.current.configuration.getString("mongoServer").get
+  val mongoPort = Play.current.configuration.getString("mongoPort").get.toInt
+  val databaseName = Play.current.configuration.getString("databaseName").get
+  val dbUserName = Play.current.configuration.getString("dbUserName").get
+  val dbUserPassword = Play.current.configuration.getString("dbUserPassword").get
 
-  /**
-   * For Local Testing
-   */
+  val mongoDB = MongoConnection(mongoServer, mongoPort)(databaseName)
+  mongoDB.authenticate(dbUserName, dbUserPassword)
 
-  val mongoDB = MongoConnection("", 27017)("beamstream")
-  mongoDB.authenticate("", "")
 
 }
