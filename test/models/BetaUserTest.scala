65fd4523232c0ca36779e80f5b86af3275653679
neelsachdeva <neelkanthsachdeva@gmail.com>
2013-05-17 17:19:55 +0530

    Google Infrastructure Integration

--- test/models/BetaUserTest.scala
+++ test/models/BetaUserTest.scala
@@ -6,7 +6,6 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.FunSuite
 import com.mongodb.casbah.commons.MongoDBObject
 import org.scalatest.junit.JUnitRunner
-import utils.GoogleDocsUploadUtility
 
 /**
  * ********************************Re-architecture ***********************************************
@@ -15,28 +14,24 @@ import utils.GoogleDocsUploadUtility
 class BetaUserTest extends FunSuite with BeforeAndAfter {
 
   before {
-//    BetaUserDAO.remove(MongoDBObject("emailId" -> ".*".r))
+    BetaUserDAO.remove(MongoDBObject("emailId" -> ".*".r))
   }
 
-  test("Google Doc Upload Test"){
-    GoogleDocsUploadUtility.upload
+  test("Add Beta User") {
+    val betaUser = BetaUser(new ObjectId, "neelkanth@knoldus.com")
+    val betaUserId = BetaUser.addBetaUser(betaUser)
+    assert(betaUserId.get === betaUser.id)	
+    
   }
-  
-//  test("Add Beta User") {
-//    val betaUser = BetaUser(new ObjectId, "neelkanth@knoldus.com")
-//    val betaUserId = BetaUser.addBetaUser(betaUser)
-//    assert(betaUserId.get === betaUser.id)	
-//    
-//  }
-//
-//  test("Find Beta User") {
-//    val betaUser = BetaUser(new ObjectId, "neelkanth@knoldus.com")
-//    val betaUserId = BetaUser.addBetaUser(betaUser)
-//    assert(BetaUser.findBetaUserbyEmail("neelkanth@knoldus.com").size === 1)
-//  }
+
+  test("Find Beta User") {
+    val betaUser = BetaUser(new ObjectId, "neelkanth@knoldus.com")
+    val betaUserId = BetaUser.addBetaUser(betaUser)
+    assert(BetaUser.findBetaUserbyEmail("neelkanth@knoldus.com").size === 1)
+  }
 
   after {
-//    BetaUserDAO.remove(MongoDBObject("emailId" -> ".*".r))
+    BetaUserDAO.remove(MongoDBObject("emailId" -> ".*".r))
   }
 
 }
