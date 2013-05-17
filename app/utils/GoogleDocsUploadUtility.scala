52213db787a8e6c1efd2753e1aed3d4877b2ee5b
neelsachdeva <neelkanthsachdeva@gmail.com>
2013-05-17 17:35:58 +0530

    Uploading Google Docs

--- app/utils/GoogleDocsUploadUtility.scala
+++ app/utils/GoogleDocsUploadUtility.scala
@@ -15,15 +15,16 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.util.Arrays
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import play.api.libs.ws.WS
 
 object GoogleDocsUploadUtility {
 
   val CLIENT_ID = "612772830843.apps.googleusercontent.com";
-  val CLIENT_SECRET = "7tTkGI2KaDX901Ngwe91Kz_K";
+  val CLIENT_SECRET = "";
 
   val REDIRECT_URI = "http://localhost:9000/oauth2callback";
 
-  def upload {
+  def UploadToGoogleDrive(fileToBeuploaded: File) {
 
     val httpTransport = new NetHttpTransport
     val jsonFactory = new JacksonFactory
@@ -31,31 +32,39 @@ object GoogleDocsUploadUtility {
     val flow = new GoogleAuthorizationCodeFlow.Builder(
       httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
       .setAccessType("online")
-      .setApprovalPrompt("auto").build();
+      .setApprovalPrompt("auto").build()
 
-    val url: String = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
-    System.out.println("Please open the following URL in your browser then type the authorization code:");
-    System.out.println("  " + url);
-    val br = new BufferedReader(new InputStreamReader(System.in));
-    val code: String = br.readLine();
+    val url: String = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build()
+//------- Neel-------- (WIP)//
+    /**
+     *  System generated GET call For Clicking that UI
+     */
 
-    val response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
-    val credential = new GoogleCredential().setFromTokenResponse(response);
+    val responseObtained = WS.url(url).get
+    println(responseObtained)
+//--------Neel------------
+    System.out.println("Please open the following URL in your browser then type the authorization code:")
+    System.out.println("  " + url)
+    val br = new BufferedReader(new InputStreamReader(System.in))
+    val code: String = br.readLine()
+
+    val response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute()
+    val credential = new GoogleCredential().setFromTokenResponse(response)
 
     //Create a new authorized API client
-    val service: Drive = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+    val service: Drive = new Drive.Builder(httpTransport, jsonFactory, credential).build()
 
     //Insert a file  
-    val body = new File();
-    body.setTitle("neel");
-    body.setDescription("A test document");
-    body.setMimeType("image/png");
+    val body = new File
+    body.setTitle("neel")
+    body.setDescription("A test document")
+    body.setMimeType("image/png")
 
-    val fileContent = new java.io.File("/home/neelkanth/Desktop/Screenshot from 2013-05-17 10:25:28.png");
-    val mediaContent = new FileContent("text/plain", fileContent);
+    val fileContent = new java.io.File("/home/neelkanth/Desktop/Screenshot from 2013-05-17 10:25:28.png")
+    val mediaContent = new FileContent("text/plain", fileContent)
 
-    val file = service.files().insert(body, mediaContent).execute();
-    System.out.println("File ID: " + file.getId());
+    val file = service.files().insert(body, mediaContent).execute()
+    System.out.println("File ID: " + file.getId())
   }
 
 }
