9291c341dddb440c132271fb925371af4510dc63
neelsachdeva <neelkanth@knoldus.com>
2013-06-25 17:01:42 +0530

    Chatting Implementation With Akka Actors

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -10,16 +10,24 @@ import play.api.libs.json.JsValue
 
 object ChatController extends Controller {
 
+  /**
+   * Chatting Logic
+   */
   def chat(userName: String, userId: String, chatWith: String) = WebSocket.async[JsValue] { request =>
     ChatRoom.join(userName, userId, chatWith)
   }
 
+  /**
+   * Enter In To ChatRoom
+   */
   def chatRoom = Action { implicit request =>
     val roomToJoin = request.queryString.get("chatWith")
     val user = User.getUserProfile(new ObjectId(request.session.get("userId").get))
-    if (roomToJoin == None)
-      Ok(views.html.chatRoom(user.head.firstName, user.head.id.toString, ""))
-    else Ok(views.html.chatRoom(user.head.firstName, user.head.id.toString,roomToJoin.head.head))
+     (roomToJoin == None) match {
+      case true => Ok(views.html.chatRoom(user.head.firstName, user.head.id.toString, ""))
+      case false => Ok(views.html.chatRoom(user.head.firstName, user.head.id.toString,roomToJoin.head.head))
+    }
+      
   }
 
 }
