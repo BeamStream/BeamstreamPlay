1ab38388e1ff7da379572a9ae5c05d19626d7ff6
john5223 <curran736@gmail.com>
2013-07-17 16:50:23 -0500

    Vagrant cookbook for beamstream completed

--- vagrant/cookbooks/beamstreamplay/recipes/default.rb
+++ vagrant/cookbooks/beamstreamplay/recipes/default.rb
@@ -20,10 +20,45 @@ package "openjdk-6-jdk" do
 	action :upgrade
 end
 
+package "mongodb" do
+  action :upgrade
+end
+
+package "unzip" do
+	action :upgrade
+end
+
+directory '/playframework' do
+  owner "vagrant"
+  mode "0777"
+  action :create
+end
+
+
 execute "wget_play" do
 	cwd '/tmp'
 	play_release = "play-2.0.2.zip"
 	play_url = "http://downloads.typesafe.com/releases/#{play_release}"
-	command "wget #{play_url}"
+	command "wget #{play_url} && unzip #{play_release} -d /playframework/ "
 	action :run
+end
+
+execute "chown_playframework" do
+  command "chown vagrant:vagrant /playframework -R ; chmod 777 /playframework -R"
+  action :run
+end
+
+
+template "/etc/init/beamstream.conf" do
+  source "beamstream.conf.erb"
+  owner "vagrant"
+  group "vagrant"
+  mode "0600"
+end
+
+service "beamstream" do
+  provider Chef::Provider::Service::Upstart
+  retries 0
+  retry_delay 2
+  action :start
 end
