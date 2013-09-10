13350540e1fbcb198444ccbb91ec345f8073f35e
macqueen <jessmacqueen@gmail.com>
2013-09-10 15:34:23 -0700

    deleted console log, unused code

--- public/client-new/js/view/questionStreamView.js
+++ public/client-new/js/view/questionStreamView.js
@@ -9,13 +9,10 @@ define(['baseView',
 			// 'click #streamTab a': 'tabHandler',
 			// 'click #show-info' :'showDetails'
 		},
-		// messagesPerPage: 10,
-		// pageNo: 1,
+
 		initialize: function() {
 			BaseView.prototype.initialize.apply(this, arguments);
 			
-			console.log('questionStreamView: init called!');
-
 			this.render();
 		},
 
@@ -24,44 +21,6 @@ define(['baseView',
 			this.$el.html(compiledTemplate);
 		}
 		
-		/**
-	     * show stream details on top 
-	     */
-	    // showDetails: function(eventName){
-	    // 	eventName.preventDefault();
-	    // 	$('.show-info').toggle(100);
-	    	
-	    // },
-	    // tabHandler: function(e){
-	    // 	var tabId=$(e.target).attr('href').replace('#',''), view;	    	
-
-	    // 	if(tabId=='discussionsView'){ 
-    		
-	    // 		/* fetch all messages of a stream for messageListView */
-	    // 		view = this.getViewById('messageListView');
-	    // 		if(view){
-	    // 			view.myStreams = this.getViewById('sidebar').myStreams;
-	    			
-	    // 			view.data.url="/allMessagesForAStream/"+this.getViewById('sidebar').streamId+"/date/"+view.messagesPerPage+"/"+view.pageNo+"/week";
-	    // 			view.fetch();
-	    		
-	    // 		}
-	    // 	}
-	    // 	if(tabId=="questionsView"){ 
-    		
-	    // 		/* fetch all messages of a stream for messageListView */
-	    // 		view = this.getViewById('questionListView');
-	    // 		if(view){
-	    // 			view.myStreams = this.getViewById('sidebar').myStreams;
-	    			
-	    // 			view.data.url="/getAllQuestionsForAStream/"+this.getViewById('sidebar').streamId+"/date/"+view.messagesPerPage+"/"+view.pageNo;
-	    // 			view.fetch();
-	    		
-	    // 		}
-	    // 	}
-
-
-	    // }
 		
 		
 	})
