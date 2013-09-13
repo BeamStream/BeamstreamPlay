0f959c1a72ec8c6fcf49aff93154d9bab6e8b140
macqueen <jessmacqueen@gmail.com>
2013-09-12 17:18:22 -0700

    using backbone fetch to create question stream collection

--- public/client-new/js/model/questionStream.js
+++ public/client-new/js/model/questionStream.js
@@ -1,46 +1,28 @@
 define(['baseModel', 
-	      '../collection/unansweredQuestions', 
-	      '../collection/answeredQuestions', 
-	      '../collection/myQuestions', 
-	      ], function(BaseModel, UnansweredQuestions, AnsweredQuestions, MyQuestions) {
+	      '../collection/questionStreams'
+	      ], function(BaseModel, QuestionStreams) {
 	var QuestionStream = BaseModel.extend({ 
 		objName: 'questionStream',
 
 		init: function() {
-			this.on('change:streamId', this.getQuestions);
+			this.on('change:streamId', this.createQuestionList);
 		},
 
 		setQuestionStreamId: function(streamId){
 			this.set('streamId', streamId);
 		},
 
-		getQuestions: function(){
-			console.log('from get questions', this.get('streamId'));
-			//var streamId = this.get('streamId');
-			var that = this;
-
+		createQuestionList: function(){
 			var requestURL = 'http://localhost:9000/getAllQuestionsForAStream/' + this.get('streamId') + '/rock/20/1';
-
-      // if statement prevents unecessary requests when streamId is undefined on page load
-      if(this.get('streamId')){
-				$.get(requestURL, function(data){
-	        var results = [];
-	        for(var i = 0; i < data.length; i++){
-	        	console.log(data[i].question);
-	        	results.push(data[i].question);
-	        }
-	        // that.set('sampleQuestion', results[0].questionBody);
-	        // //console.log(that.get('sampleQuestion'));
-	        // that.set('unansweredQuestions', new UnansweredQuestions());
-	        // console.log(that.get('unansweredQuestions'));
-	        // that.get('unansweredQuestions').add(results);
-	        // console.log(that.get('unansweredQuestions'));
-				});
-			}
-		}
-		// unansweredQuestions: new UnansweredQuestions,
-		// answeredQuestions: new AnsweredQuestions,
-		// myQuestions: new MyQuestions
+			this.set('questionStreams', new QuestionStreams());
+			var that = this;
+      this.get('questionStreams')
+          .fetch({url: requestURL, 
+                  success: function(){ 
+                  	console.log('coll fetch', that.get('questionStreams')); 
+                  }
+                });
+		},
 
 	});
        
