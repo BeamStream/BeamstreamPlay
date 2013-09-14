18b33d5d53ac4528447eabe1ddee3fad811dfa61
macqueen <jessmacqueen@gmail.com>
2013-09-14 11:09:16 -0700

    added current stream property and method to update it

--- public/client-new/js/model/questionStream.js
+++ public/client-new/js/model/questionStream.js
@@ -7,10 +7,32 @@ define(['baseModel',
 		init: function() {
 			this.on('change:streamId', this.createQuestionList);
 			this.set('questionStreams', new QuestionStreams());
+			this.set('currentQuestionStream', new QuestionStreams());
+			//this.on('change:questionStreams', this.updateCurrentStream());
+			console.log(this);
 		},
 
 		setQuestionStreamId: function(streamId){
 			this.set('streamId', streamId);
+		},
+
+		updateCurrentStream: function(){
+			var that = this;
+
+      var updatedStream = that.get('questionStreams').filter(function(model){
+      	if(that.get('currentFilter') === 'unanswered'){
+      		return model.get('question').answered === false;
+      	}
+      	if(that.get('currentFilter') === 'answered'){
+      		return model.get('question').answered === true;
+      	}
+      	if(that.get('currentFilter') === 'myQuestions'){
+      		// TODO: figure out logged in user
+      		return false;
+      	}
+      });
+      this.get('currentQuestionStream').reset(updatedStream);
+      console.log('this is the current stream', this.get('currentQuestionStream'));
 		},
 
 		createQuestionList: function(){
@@ -19,10 +41,11 @@ define(['baseModel',
       this.get('questionStreams')
           .fetch({url: requestURL, 
                   success: function(){ 
+                  	that.updateCurrentStream();
                   	console.log('coll fetch', that.get('questionStreams')); 
                   }
                 });
-		},
+		}
 
 	});
        
