f3d43848e25312f0fc2af2bb8714f0266d7f6ad9
macqueen <jessmacqueen@gmail.com>
2013-09-16 10:10:04 -0700

    added current logged in user to question stream

--- public/client-new/js/model/questionStream.js
+++ public/client-new/js/model/questionStream.js
@@ -1,6 +1,7 @@
 define(['baseModel', 
-	      '../collection/questionStreams'
-	      ], function(BaseModel, QuestionStreams) {
+				'../collection/questionStreams', 
+				'model/onlineuser'
+				], function(BaseModel, QuestionStreams, OnlineUser) {
 	var QuestionStream = BaseModel.extend({ 
 		objName: 'questionStream',
 
@@ -9,7 +10,16 @@ define(['baseModel',
 			this.set('questionStreams', new QuestionStreams());
 			this.set('currentQuestionStream', new QuestionStreams());
 			//this.on('change:currentFilter', this.updateCurrentStream());
+			this.setLoggedInUser();
 			console.log(this);
+		},
+
+		setLoggedInUser: function(){
+			this.set('onlineUser', new OnlineUser());
+			var that = this;
+			var requestURL = 'http://localhost:9000/loggedInUserJson';
+			this.get('onlineUser')
+				.fetch({url: requestURL});
 		},
 
 		setQuestionStreamId: function(streamId){
@@ -23,35 +33,35 @@ define(['baseModel',
 		updateCurrentStream: function(){
 			var that = this;
 
-      var updatedStream = that.get('questionStreams').filter(function(model){
-      	if(that.get('currentFilter') === 'unanswered'){
-      		return model.get('question').answered === false;
-      	}
-      	if(that.get('currentFilter') === 'answered'){
-      		return model.get('question').answered === true;
-      	}
-      	if(that.get('currentFilter') === 'myQuestions'){
-      		// TODO: figure out logged in user
-      		return false;
-      	}
-      });
-      this.get('currentQuestionStream').reset(updatedStream);
-      console.log('this is the current stream', this.get('currentQuestionStream'));
+			var updatedStream = that.get('questionStreams').filter(function(model){
+				if(that.get('currentFilter') === 'unanswered'){
+					return model.get('question').answered === false;
+				}
+				if(that.get('currentFilter') === 'answered'){
+					return model.get('question').answered === true;
+				}
+				if(that.get('currentFilter') === 'myQuestions'){
+					// TODO: figure out logged in user
+					return false;
+				}
+			});
+			this.get('currentQuestionStream').reset(updatedStream);
+			console.log('this is the current stream', this.get('currentQuestionStream'));
 		},
 
 		createQuestionList: function(){
 			var requestURL = 'http://localhost:9000/getAllQuestionsForAStream/' + this.get('streamId') + '/rock/20/1';
 			var that = this;
-      this.get('questionStreams')
-          .fetch({url: requestURL, 
-                  success: function(){ 
-                  	that.updateCurrentStream();
-                  	console.log('coll fetch', that.get('questionStreams')); 
-                  }
-                });
+			this.get('questionStreams')
+					.fetch({url: requestURL, 
+									success: function(){ 
+										that.updateCurrentStream();
+										console.log('coll fetch', that.get('questionStreams')); 
+									}
+								});
 		}
 
 	});
-       
+			 
 	return QuestionStream;
 });
