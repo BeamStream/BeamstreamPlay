3e240cbe6722ed01d23cc912a0c4b6d1fc79c0e2
macqueen <jessmacqueen@gmail.com>
2013-09-16 17:16:43 -0700

    added comparator so that questions will be added in order of how many rocks they have

--- public/client-new/js/collection/questionStreams.js
+++ public/client-new/js/collection/questionStreams.js
@@ -5,6 +5,10 @@ define([
 		model: Question,
 		objName: 'questionStreams',
 
+		comparator: function(question){
+			return - question.get('question').rockers.length;
+		}
+
 	});
 
 	return QuestionStreams;
