2d3cafc1b40d0d42d68a589044f2d6ce82e5f885
macqueen <jessmacqueen@gmail.com>
2013-09-23 19:03:28 -0700

    added eventing for editstatus and a smarter update function that is not in use yet

--- public/client-new/js/collection/questionStreams.js
+++ public/client-new/js/collection/questionStreams.js
@@ -5,8 +5,32 @@ define([
 		model: Question,
 		objName: 'questionStreams',
 
+		initialize: function(){
+			this.on('change:editStatus', function(question){
+				this.trigger('statusChange', {editStatus: question.get('editStatus')});
+			}, this);
+		},
+
 		comparator: function(question){
 			return - question.get('question').rockers.length;
+		}, 
+
+		updateCollection: function(models){
+			var that = this;
+			var counter = 0;
+			_.each(models, function(newQuestion){
+				that.each(function(currentQuestion){
+					if (currentQuestion.get('question').id.id === newQuestion.get('question').id.id &&
+							currentQuestion.get('comments').length === newQuestion.get('comments').length &&
+							currentQuestion.get('question').answers.length === newQuestion.get('question').answers.length &&
+							currentQuestion.get('question').rockers.length === newQuestion.get('question').rockers.length){
+						counter++;
+					} else if (counter === that.length - 1) {
+						that.add(newQuestion);
+						counter = 0;
+					}
+				})
+			})
 		}
 
 	});
