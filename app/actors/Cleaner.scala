d1d885787532921dd80bb58fed544c9f6ce17473
neelsachdeva <neelkanth@knoldus.com>
2013-12-06 10:36:56 +0530

    Cleanr Actor

---
+++ app/actors/Cleaner.scala
@@ -0,0 +1,37 @@
+package actors
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import utils.OnlineUserCache
+import scala.concurrent.duration._
+
+class CleanerActor extends Actor {
+  def receive = {
+    case "clean" =>
+      val utcMilliseconds = OnlineUserCache.returnUTCTime
+      val onlineUsersMap = OnlineUserCache.returnOnlineUsers.head.onlineUsers
+      onlineUsersMap.foreach {
+        a =>
+          if (utcMilliseconds - a._2 > 10000) {
+            OnlineUserCache.setOffline(a._1)
+          }
+      }
+  }
+}
+object Cleaner {
+
+  val system = ActorSystem("mySystem")
+  val myActor = system.actorOf(Props[CleanerActor], "cleanerActor")
+
+  import system.dispatcher
+  def makeUsersOfflineIfNotAvailable = {
+
+    val cancellable =
+      system.scheduler.schedule(10000 milliseconds,
+        10000 milliseconds,
+        myActor,
+        "clean")
+  }
+
+}
