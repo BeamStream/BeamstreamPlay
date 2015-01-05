6983c2d349d33c4e184fa91a5f1b927e9fd64554
maltiyadav <maltiyadava@gmail.com>
2015-01-05 17:45:27 +0530

    change mongo connection

--- app/utils/MongoHQConfig.scala
+++ app/utils/MongoHQConfig.scala
@@ -9,15 +9,16 @@ object MongoHQConfig {
    * These all will come from "application.conf" file
    *
    */
-  val mongoServer = Play.current.configuration.getString("mongoServer").get
+  /*val mongoServer = Play.current.configuration.getString("mongoServer").get
   val mongoPort = Play.current.configuration.getString("mongoPort").get.toInt
   val databaseName = Play.current.configuration.getString("databaseName").get
   val dbUserName = Play.current.configuration.getString("dbUserName").get
   val dbUserPassword = Play.current.configuration.getString("dbUserPassword").get
   val mongoDB = MongoConnection(mongoServer, mongoPort)(databaseName)
-  mongoDB.authenticate(dbUserName, dbUserPassword)
+  mongoDB.authenticate(dbUserName, dbUserPassword)*/
 
   /*val mongoDB = MongoConnection("", 27017)("beamstream-test")
   mongoDB.authenticate("neel", "neel")*/
-
+  val mongoDB = MongoConnection("", 27017)("beamstream")
+  mongoDB.authenticate("neel", "neel")
 }
