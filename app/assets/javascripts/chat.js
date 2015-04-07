b94fe39e091745af28442ca40d0cdf62f59bc513
Malti Yadav <malti@knoldus.com>
2015-04-07 12:27:22 +0530

    #1495-set the picture of the user who initiate the chat

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -28,7 +28,10 @@ function startChat(userId) {
 							+ '<div class="right-border"></div>'
 							+ '</div>'
 							+ '<div class="chatbox-footer">'
-							+ '<div class="my-avatar"></div>'
+							+ '<div class="my-avatar">'
+							+ "<img src=" +localStorage["loggedUserProfileUrl"]
+							+ ">"
+							+'</div>'
 							+ '<textarea id="talk" name="styled-textarea" class="message" placeholder="Type message here...">'
 							+ '</textarea>'
 							+ '<p>Press enter to submit message</p>' + '</div>'
@@ -105,139 +108,106 @@ function startChat(userId) {
 	};
 }
 
-function popit(userId, toWhom, name, profileImageUrl) {
+function popit(userId, toWhom, name) {
 	if(document.querySelector('#chat-' + toWhom) == null){
-	
-	/*$
-			.ajax({
-				url : '/canStartChat/ask/' + userId + "/" + toWhom,
-				success : function(data) {
-					if (data == "true") {*/
-						var newChatSocket = new WebSocket('ws://localhost:9000/startChat/' + userId + "/"+ toWhom)
-						var itsId = "chat" + "-" + toWhom;
-						$(".chatbox")
-								.append(
-										"<div id="
-												+ itsId
-												+ ">"
-												+ '<div class="chatbox-header">'
+			var newChatSocket = new WebSocket('ws://localhost:9000/startChat/' + userId + "/"+ toWhom)
+			var itsId = "chat" + "-" + toWhom;
+			$(".chatbox")
+					.append(
+							"<div id="
+									+ itsId
+									+ ">"
+									+ '<div class="chatbox-header">'
 
-												+ '<h1 class="friend"></h1>'
+									+ '<h1 class="friend"></h1>'
 
-												+ '<div class="exit"></div>'
+									+ '<div class="exit"></div>'
 
-												+ '<div class="addfriends-btn"><a href="#"></a></div>'
-												+ '</div>'
+									+ '<div class="addfriends-btn"><a href="#"></a></div>'
+									+ '</div>'
 
-												+ '<div class="chatbox-body">'
-												+ '<div class="chatbox-content">'
+									+ '<div class="chatbox-body">'
+									+ '<div class="chatbox-content">'
 
-												+ '<div class="chat-feed" id="main">'
-												+ '<div id="messages"></div>'
-												+ '</div>'
+									+ '<div class="chat-feed" id="main">'
+									+ '<div id="messages"></div>'
+									+ '</div>'
 
-												+ '</div>'
+									+ '</div>'
 
-												+ '<div class="left-border"></div>'
-												+ '<div class="right-border"></div>'
-												+ '</div>'
-												+ '<div class="chatbox-footer">'
-												+ '<div class="my-avatar"></div>'
-												+ '<textarea id="talk" name="styled-textarea" class="message" placeholder="Type message here...">'
-												+ '</textarea>'
-												+ '<p>Press enter to submit message</p>'
-												+ '</div>' + '</div>');
-						$("div#" + itsId + " " + "h1.friend").text(name);
-						$(".chatbox").css("display", "block");
-						$("#" + itsId).css("display", "block");
-						$(".chatbox").css("position", "fixed");
-						$(".chatbox").css("bottom", "0");
-						$(".chatbox").css("right", "322");
-						var newSendMessage = function() {
-							newChatSocket.send(JSON.stringify({
-								text : $("#" + itsId + " " + "textarea#talk")
-										.val()
-							}))
-							$("div#" + itsId + " " + "textarea#talk").val('')
-						}
+									+ '<div class="left-border"></div>'
+									+ '<div class="right-border"></div>'
+									+ '</div>'
+									+ '<div class="chatbox-footer">'
+									+ '<div class="my-avatar">'
+									+ "<img src=" +localStorage["loggedUserProfileUrl"]
+									+ ">"
+									+'</div>'
+									+ '<textarea id="talk" name="styled-textarea" class="message" placeholder="Type message here...">'
+									+ '</textarea>'
+									+ '<p>Press enter to submit message</p>'
+									+ '</div>' + '</div>');
+			$("div#" + itsId + " " + "h1.friend").text(name);
+			$(".chatbox").css("display", "block");
+			$("#" + itsId).css("display", "block");
+			$(".chatbox").css("position", "fixed");
+			$(".chatbox").css("bottom", "0");
+			$(".chatbox").css("right", "322");
+			var newSendMessage = function() {
+				newChatSocket.send(JSON.stringify({
+					text : $("#" + itsId + " " + "textarea#talk")
+							.val()
+				}))
+				$("div#" + itsId + " " + "textarea#talk").val('')
+			}
 
-						var newReceiveEvent = function(event) {
-							var data = JSON.parse(event.data)
-							if (data.message == "ping") {
-								return;
-							} else {
-								if (data.error) {
-									newChatSocket.close()
-									$("#onError span").text(data.error)
-									$("#onError").show()
-								}
-								if (data.kind == "quit") {
-									$("#" + itsId).remove();
+			var newReceiveEvent = function(event) {
+				var data = JSON.parse(event.data)
+				if (data.message == "ping") {
+					return;
+				} else {
+					if (data.error) {
+						newChatSocket.close()
+						$("#onError span").text(data.error)
+						$("#onError").show()
+					}
+					if (data.kind == "quit") {
+						$("#" + itsId).remove();
+						newChatSocket.close();
+						return;
+					}
 
-									/*$.ajax({
-										url : '/canStartChat/""/' + userId
-												+ "/" + itsId,
-										success : function(data) {
-											console.log(data)
-										}
-									});*/
-									newChatSocket.close();
-									return;
-								}
+					var el = $('<div class="message"><span></span><p></p></div>')
+					$("span", el).text(data.user)
+					$("p", el).text(": " + data.message)
+					$(el).addClass(data.kind)
+					$("#" + itsId + " " + "div#messages").append(el)
+				}
+			}
 
-								// Create the message element
-								var el = $('<div class="message"><span></span><p></p></div>')
-								$("span", el).text(data.user)
-								$("p", el).text(": " + data.message)
-								$(el).addClass(data.kind)
-								$("#" + itsId + " " + "div#messages")
-										.append(el)
-							}
-						}
+			var newHandleReturnKey = function(e) {
+				if (e.charCode == 13 || e.keyCode == 13) {
+					e.preventDefault()
+					newSendMessage()
+				}
+			}
+			$("#" + itsId + " " + "textarea#talk").keypress(newHandleReturnKey)
 
-						var newHandleReturnKey = function(e) {
-							if (e.charCode == 13 || e.keyCode == 13) {
-								e.preventDefault()
-								newSendMessage()
-							}
-						}
-						$("#" + itsId + " " + "textarea#talk").keypress(
-								newHandleReturnKey)
+			newChatSocket.onmessage = newReceiveEvent
 
-						newChatSocket.onmessage = newReceiveEvent
+			$("#" + itsId + " " + "div.chatbox-header div.exit").click(function() {
+					$("#" + itsId).remove();
+					newChatSocket.close();
+			});
 
-						$("#" + itsId + " " + "div.chatbox-header div.exit")
-								.click(
-										function() {
-											$("#" + itsId).remove();
-											newChatSocket.close();
-											/*$
-													.ajax({
-														url : '/canStartChat/""/'
-																+ userId
-																+ "/"
-																+ itsId,
-														success : function(data) {
-															console.log(data)
-														}
-													});*/
-										});
-
-						newChatSocket.onopen = function() {
-							console.log('websocket opened');
-							setInterval(function() {
-								if (newChatSocket.bufferedAmount == 0)
-									newChatSocket.send(JSON.stringify({
-										text : "ping"
-									}))
-							}, 30000);
-						};
-					/*}
-
-					else {
-						alert("You've already got a connection opened with this user")
-					}*/
-
-			
+			newChatSocket.onopen = function() {
+				console.log('websocket opened');
+				setInterval(function() {
+					if (newChatSocket.bufferedAmount == 0)
+						newChatSocket.send(JSON.stringify({
+							text : "ping"
+						}))
+				}, 30000);
+			};
 	}
 }
--- public/client-new/js/view/onlineUserView.js
+++ public/client-new/js/view/onlineUserView.js
@@ -34,9 +34,6 @@ define(
 						onAfterInit : function() {
 							this.data.reset();
 							this.scroll();
-							// this.pushConnection();
-							// this.pushConnectionOffline();
-
 						},
 
 						/**
@@ -50,10 +47,8 @@ define(
 						 * if there is no other online users
 						 */
 						displayNoResult : function(callback) {
-							var compiledTemplate = Handlebars
-									.compile(OnlineUsers);
-							this.$(".content").html(
-									compiledTemplate(this.data.toJSON()));
+							var compiledTemplate = Handlebars.compile(OnlineUsers);
+							this.$(".content").html(compiledTemplate(this.data.toJSON()));
 						},
 
 						/**
@@ -61,80 +56,62 @@ define(
 						 */
 						displayPage : function() {
 
-							$
-									.ajax({
-										url : '/onlineUsers',
-										success : function(data) {
+							$.ajax({
+									url : '/onlineUsers',
+									success : function(data) {
+										var self = this;
+										$('#user-online ul').html("");
+										var onlineUsersLength = data.onlineUsers.length;
+										$('.online-count').html(
+											"Online ("
+													+ onlineUsersLength 
+													+ ")"
+										);
+										_.each(data.onlineUsers,function(model) {
+											var profileImageUrl = '';
+											if (model.profileImageUrl) {
 
-											var self = this;
-											$('#user-online ul').html("");
-											var onlineUsersLength = data.onlineUsers.length;
-											$('.online-count')
+												profileImageUrl = model.profileImageUrl;
+
+											} else {
+
+												profileImageUrl = '/beamstream-new/images/profile-upload.png';
+											}
+
+											if (model.id.id == localStorage["loggedUserId"] ) {
+												var template = '<li id="me" class="online active"><a href="#" class="active"><img src="'
+														+ profileImageUrl
+														+ '" width="30" height="28"> '
+														+ '<span>Me</span> <span class="online-chat">Online</span></a></li>';
+
+												$('#user-online ul').prepend(template);
+											} else {
+
+												var template = '<li id="'
+														+ model.id.id
+														+ '" onclick=popit("'
+														+ localStorage["loggedUserId"]
+														+ '","'
+														+ model.id.id
+														+ '","'
+														+ model.firstName
+														+ '")> '
+														+ '<img width="30" height="28" src="'
+														+ profileImageUrl
+														+ '">'
+														+ '<span>'
+														+ model.firstName
+														+ '</span> <span class="online-chat">Online</span>';
+
+												$('#user-online ul').append(template);
+
+											}
 											
-													.html(
-															"Online ("
-																	+ onlineUsersLength // this.data.models[0].attributes.onlineUsers.length
-																	+ ")");
-											_
-													.each(
-															data.onlineUsers,// this.data.models[0].attributes.onlineUsers,
-															function(model) {
-																var profileImageUrl = '';
-																if (model.profileImageUrl) {
+											$('#user-online').mCustomScrollbar("update");
 
-																	profileImageUrl = model.profileImageUrl;
-
-																} else {
-
-																	profileImageUrl = '/beamstream-new/images/profile-upload.png';
-																}
-
-																if (model.id.id == localStorage["loggedUserId"] /*&& document.querySelector('#me') == null*/) {
-																	var template = '<li id="me" class="online active"><a href="#" class="active"><img src="'
-																			+ profileImageUrl
-																			+ '" width="30" height="28"> '
-																			+ '<span>Me</span> <span class="online-chat">Online</span></a></li>';
-
-																	$(
-																			'#user-online ul')
-																			.prepend(
-																					template);
-																} else /*if(model.id.id != localStorage["loggedUserId"] && document.getElementById(model.id.id) == null)*/{
-
-																	var template = '<li id="'
-																			+ model.id.id
-																			+ '" onclick=popit("'
-																			+ localStorage["loggedUserId"]
-																			+ '","'
-																			+ model.id.id
-																			+ '","'
-																			+ model.firstName
-																			+ '","'
-																			+ profileImageUrl
-																			+ '")> '
-																			+ '<img width="30" height="28" src="'
-																			+ profileImageUrl
-																			+ '">'
-																			+ '<span>'
-																			+ model.firstName
-																			+ '</span> <span class="online-chat">Online</span>';
-
-																	$(
-																			'#user-online ul')
-																			.append(
-																					template);
-
-																}
-																
-																$(
-																		'#user-online')
-																		.mCustomScrollbar(
-																				"update");
-
-															});
-
-										}
-									});
+										});
+									}
+							});
 						},
 
 						onAfterRender : function() {
@@ -144,149 +121,38 @@ define(
 							}, 10*1000);
 						},
 
-						/* Pubnub subscription for online user */
-						// TODO : PubNUb Problem For Online Users , Needs to be
-						// checked
-						/*
-						 * pushConnection: function(){ var self = this;
-						 * self.pagePushUid =
-						 * Math.floor(Math.random()*16777215).toString(16);
-						 * 
-						 * for online users PUBNUB.subscribe({ channel :
-						 * "onlineUsers",
-						 * 
-						 * callback : function(message) { if(message.pagePushUid !=
-						 * self.pagePushUid) { var template='<li id="'+message.userInfo.user.id.id+'" onclick=popit("'+localStorage["loggedUserId"]+'","'+message.userInfo.user.id.id+'","'+message.userInfo.user.firstName+'","'+ profileImageUrl +'")> '+
-						 * message.userInfo.user.firstName +'<span
-						 * class="online-chat"><img width="12" height="13"
-						 * src="/beamstream-new/images/online-icon.png"></span></li>';
-						 * $('#user-online ul').append(template); //
-						 * $('#user-online ul').append('<li>Ankit</li>'); } } }) },
-						 * 
-						 * 
-						 * Pubnub subscription for offline user
-						 * pushConnectionOffline: function(){ var self = this;
-						 * self.pagePushUid =
-						 * Math.floor(Math.random()*16777215).toString(16);
-						 * 
-						 * for online users PUBNUB.subscribe({ channel :
-						 * "offlineuser", callback : function(message) { //
-						 * $('div#user-online ul li').empty(); } }) },
-						 * 
-						 */
 
 						/**
 						 * method to provide scrolling functionality in
 						 * onlineusers box
 						 */
 						scroll : function(eventName) {
-
 							$("#user-online").mCustomScrollbar({
-								set_width : false, /*
-													 * optional element width:
-													 * boolean, pixels,
-													 * percentage
-													 */
-								set_height : false, /*
-													 * optional element height:
-													 * boolean, pixels,
-													 * percentage
-													 */
-								horizontalScroll : false, /*
-															 * scroll
-															 * horizontally:
-															 * boolean
-															 */
-								scrollInertia : 550, /*
-														 * scrolling inertia:
-														 * integer
-														 * (milliseconds)
-														 */
-								scrollEasing : "easeOutCirc", /*
-																 * scrolling
-																 * easing:
-																 * string
-																 */
-								mouseWheel : "auto", /*
-														 * mousewheel support
-														 * and velocity:
-														 * boolean, "auto",
-														 * integer
-														 */
-								autoDraggerLength : true, /*
-															 * auto-adjust
-															 * scrollbar dragger
-															 * length: boolean
-															 */
-								scrollButtons : { /* scroll buttons */
-									enable : false, /*
-													 * scroll buttons support:
-													 * boolean
-													 */
-									scrollType : "continuous", /*
-																 * scroll
-																 * buttons
-																 * scrolling
-																 * type:
-																 * "continuous",
-																 * "pixels"
-																 */
-									scrollSpeed : 20, /*
-														 * scroll buttons
-														 * continuous scrolling
-														 * speed: integer
-														 */
+								set_width : false, 
+								set_height : false, 
+								horizontalScroll : false,
+								scrollInertia : 550, 
+								scrollEasing : "easeOutCirc", 
+								mouseWheel : "auto",
+								autoDraggerLength : true, 
+								scrollButtons : { 
+									enable : false, 
+									scrollType : "continuous", 
+									scrollSpeed : 20,
 									scrollAmount : 40
-								/*
-								 * scroll buttons pixels scroll amount: integer
-								 * (pixels)
-								 */
 								},
 								advanced : {
-									updateOnBrowserResize : true, /*
-																	 * update
-																	 * scrollbars
-																	 * on
-																	 * browser
-																	 * resize
-																	 * (for
-																	 * layouts
-																	 * based on
-																	 * percentages):
-																	 * boolean
-																	 */
-									updateOnContentResize : false, /*
-																	 * auto-update
-																	 * scrollbars
-																	 * on
-																	 * content
-																	 * resize
-																	 * (for
-																	 * dynamic
-																	 * content):
-																	 * boolean
-																	 */
+									updateOnBrowserResize : true,
+									updateOnContentResize : false,
 									autoExpandHorizontalScroll : false
-								/*
-								 * auto expand width for horizontal scrolling:
-								 * boolean
-								 */
 								},
 								callbacks : {
 									onScroll : function() {
-									}, /*
-										 * user custom callback function on
-										 * scroll event
-										 */
+									},
 									onTotalScroll : function() {
-									}, /*
-										 * user custom callback function on
-										 * bottom reached event
-										 */
+									},
 									onTotalScrollOffset : 0
-								/*
-								 * bottom reached offset: integer (pixels)
-								 */
+								
 								}
 							});
 						},
