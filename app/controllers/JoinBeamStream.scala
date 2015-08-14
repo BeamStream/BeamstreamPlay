2a6f3fc7b9ac3bb55e638dc9736ee1e49d03dfae
Malti Yadav <malti@knoldus.com>
2015-08-14 18:08:53 +0530

    create route for image uploader

--- app/controllers/JoinBeamStream.scala
+++ app/controllers/JoinBeamStream.scala
@@ -45,6 +45,10 @@ object JoinBeamStream extends Controller {
     }
 
   }
+  
+  def imageUploader: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.imageUploader("Image Uploader"))
+  }
 
   /**
    *  Beta Users Registration(T)
