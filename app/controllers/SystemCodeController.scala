90ca5b5a7f09aee189a183d56a4768e122defde4
Malti Yadav <malti@knoldus.com>
2015-09-07 19:02:29 +0530

    Implemented number code system

--- app/controllers/SystemCodeController.scala
+++ app/controllers/SystemCodeController.scala
@@ -3,11 +3,13 @@ package controllers
 import play.api.mvc._
 import org.bson.types.ObjectId
 import models.SystemCode
+import play.api.libs.json.Json
 
 object SystemCodeController extends Controller {
 
   def systemCode: Action[AnyContent] = Action { implicit request =>
-     Ok(views.html.systemcode("create system code"))
+     val systemCode = SystemCode.findAll
+     Ok(views.html.systemcode("create system code", systemCode))
   }
   
   def createSystemCode: Action[AnyContent] = Action { implicit request =>
@@ -16,6 +18,26 @@ object SystemCodeController extends Controller {
     val systemCodeObj = SystemCode(new ObjectId,systemCode,true)
     SystemCode.createSystemCode(systemCodeObj)
     Redirect("/systemCode")
+  }
+  
+  def updateSystemCode: Action[AnyContent] = Action { implicit request =>
+    SystemCode.update(new ObjectId("55ed5c43c83007f8137a296a"),false)
+    Redirect("/systemCode")
+  }
+  
+  def changeSystemCodeStatus: Action[AnyContent] = Action { implicit request =>
+    val data = request.body.asFormUrlEncoded.get
+    val status = data("status").toList(0)
+    val id = data("id").toList(0)
+    SystemCode.update(new ObjectId(id), status.toBoolean)
+    Ok(Json.obj("status" -> "success")).as("application/json")
+  }
+  
+  def deleteSystemCode: Action[AnyContent] = Action { implicit request =>
+    val data = request.body.asFormUrlEncoded.get
+    val id = data("id").toList(0)
+    SystemCode.removeById(new ObjectId(id))
+    Ok(Json.obj("status" -> "success")).as("application/json")
   }
 
 }
--- app/models/SystemCode.scala
+++ app/models/SystemCode.scala
@@ -16,11 +16,31 @@ object SystemCode {
   /**
    * Create a System Code
    */
-  def createSystemCode(systemCode: SystemCode): Option[ObjectId] = {
-    SystemCodeDAO.insert(systemCode)
+  def createSystemCode(systemCode: SystemCode): Option[ObjectId] = SystemCodeDAO.insert(systemCode)
+  
+  /**
+   * Get all system code
+   */
+  def findAll:List[SystemCode] = SystemCodeDAO.find(MongoDBObject.empty).toList
+  
+  /**
+   * Get all system code
+   */
+  def finOneById(id : ObjectId):Option[SystemCode] = SystemCodeDAO.findOneById(id)
+  
+  /**
+   * update the code system
+   */
+  def update(id : ObjectId, status : Boolean):Unit = {
+    val obj = finOneById(id).get
+    val updateObj = MongoDBObject("_id" -> id ,"code" -> obj.code, "status" -> status )
+    SystemCodeDAO.update(MongoDBObject("_id" -> id),updateObj, false, false, new WriteConcern)
   }
   
- 
+  /**
+   * Remove system code
+   */
+  def removeById(id : ObjectId):Unit = SystemCodeDAO.removeById(id)
 }
-
+ 
 object SystemCodeDAO extends SalatDAO[SystemCode, ObjectId](collection = MongoHQConfig.mongoDB("system_code"))
